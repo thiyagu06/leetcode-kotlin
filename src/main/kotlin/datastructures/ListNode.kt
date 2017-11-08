37e23127c987c476298a5c00c8b846596ea8531d
Navid Rojiani <nrojiani@gmail.com>
2017-11-08 00:19:54 -0800

    Add ListNode & TreeNode

---
+++ src/main/kotlin/datastructures/ListNode.kt
@@ -0,0 +1,8 @@
+package datastructures
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode(var `val`: Int = 0) {
+    var next: ListNode? = null
+}
---
+++ src/main/kotlin/datastructures/TreeNode.kt
@@ -0,0 +1,9 @@
+package datastructures
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode(var `val`: Int = 0) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
