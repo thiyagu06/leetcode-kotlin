636cbc821bd5d367bbfba87bdd39aa09b8ebed0d
Navid Rojiani <nrojiani@gmail.com>
2017-11-08 00:15:23 -0800

    344

---
+++ src/main/kotlin/algorithms/easy/_344_reverse_string/ReverseString.kt
@@ -0,0 +1,27 @@
+package algorithms.easy._344_reverse_string
+
+/**
+ * 344 - https://leetcode.com/problems/reverse-string/description/
+ *
+ * @author nrojiani
+ * @date 11/8/17
+ */
+class Solution {
+    /*
+     The easy way:
+     fun reverseString(s: String): String = s.reversed()
+     */
+
+    fun reverseString(s: String): String {
+        var chars = s.toCharArray()
+        var (i, j) = (0 to s.lastIndex)
+        while (i < j) {
+            val temp = chars[i]
+            chars[i] = chars[j]
+            chars[j] = temp
+            i++
+            j--
+        }
+        return chars.joinToString("")
+    }
+}
