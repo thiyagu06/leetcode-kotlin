15f99a0a97b5fd8aa6d56e813158c3fd4efc08db
Navid Rojiani <nrojiani@gmail.com>
2017-11-24 19:00:03 -0800

    88

---
+++ src/main/kotlin/algorithms/easy/_88_merge_sorted_array/MergeSortedArray.kt
@@ -0,0 +1,45 @@
+package algorithms.easy._88_merge_sorted_array
+
+/**
+ * 88 - https://leetcode.com/problems/merge-sorted-array/description/
+ *
+ * Note:
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements
+ * from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+ *
+ * @author nrojiani
+ * @date 11/24/17
+ */
+class Solution {
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+        var aux1 = IntArray(m)
+        for (i in 0 until m) {
+            aux1[i] = nums1[i]
+        }
+
+        var idx1 = 0
+        var idx2 = 0
+        var mIdx = 0
+
+        while (idx1 < m && idx2 < n) {
+            if (aux1[idx1] <= nums2[idx2]) {
+                nums1[mIdx++] = aux1[idx1++]
+            } else {
+                nums1[mIdx++] = nums2[idx2++]
+            }
+        }
+
+        while (idx1 < m) {
+            nums1[mIdx++] = aux1[idx1++]
+        }
+
+        while (idx2 < n) {
+            nums1[mIdx++] = nums2[idx2++]
+        }
+
+    }
+}
