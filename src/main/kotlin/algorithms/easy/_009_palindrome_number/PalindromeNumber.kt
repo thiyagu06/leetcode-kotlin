f2b75d13d95fce52dfdf831c7ded6b2416c6d47b
Navid Rojiani <nrojiani@gmail.com>
2017-11-30 18:19:04 -0800

    9

---
+++ src/main/kotlin/algorithms/easy/_009_palindrome_number/PalindromeNumber.kt
@@ -0,0 +1,27 @@
+package algorithms.easy._009_palindrome_number
+
+import extensions.digits
+
+/**
+ * 9 - https://leetcode.com/problems/palindrome-number/description/
+ *
+ * Note: Negative numbers not considered palindromes.
+ */
+class Solution {
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+
+        val digits = x.digits()
+        var middle = digits
+        while (middle.isNotEmpty()) {
+            if (middle.first() != middle.last())
+                return false
+            middle = middle.drop(1).dropLast(1)
+        }
+        return true
+    }
+}
--- src/test/kotlin/algorithms/easy/_009_palindrome_number/PalindromeNumberTest.kt
+++ src/test/kotlin/algorithms/easy/_009_palindrome_number/PalindromeNumberTest.kt
@@ -8,8 +8,24 @@ import org.junit.Assert.*
  *
  */
 class PalindromeNumberTest {
+    private val solution = Solution()
+
     @Test
     fun isPalindrome() {
+        assertTrue(solution.isPalindrome(1))
+        assertTrue(solution.isPalindrome(11))
+        assertTrue(solution.isPalindrome(121))
+        assertTrue(solution.isPalindrome(1221))
+        assertTrue(solution.isPalindrome(12321))
+        assertTrue(solution.isPalindrome(123454321))
+        assertFalse(solution.isPalindrome(-1))
+        assertFalse(solution.isPalindrome(12))
+    }
+
+    @Test
+    fun isPalindromeOverflow() {
+        assertFalse(solution.isPalindrome(Int.MIN_VALUE))
+        assertFalse(solution.isPalindrome(Int.MAX_VALUE))
     }
 
 }
