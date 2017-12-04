d294ae0cb99d370aa1dc5b6c0d5488744ca9deb0
Navid Rojiani <nrojiani@gmail.com>
2017-12-04 00:34:28 -0800

    283

---
+++ src/main/kotlin/algorithms/easy/_283_move_zeroes/MoveZeroes.kt
@@ -0,0 +1,26 @@
+package algorithms.easy._283_move_zeroes
+
+import extensions.swap
+
+/**
+ * 283 - https://leetcode.com/problems/move-zeroes/description/
+ */
+class Solution {
+    /**
+     * Time: O(n)
+     * Space: O(1)
+     */
+    fun moveZeroes(nums: IntArray) {
+        nums.withIndex().forEach { (i, n) ->
+            if (n == 0) {
+                var j = i + 1
+                while (j <= nums.lastIndex && nums[j] == 0)
+                    j++
+
+                if (j > nums.lastIndex) return
+
+                nums.swap(i, j)
+            }
+        }
+    }
+}
--- src/test/kotlin/algorithms/easy/_283_move_zeroes/MoveZeroesTest.kt
+++ src/test/kotlin/algorithms/easy/_283_move_zeroes/MoveZeroesTest.kt
@@ -4,12 +4,25 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- *
- */
 class MoveZeroesTest {
+    private val solution = Solution()
+
     @Test
     fun moveZeroes() {
+        val expectedInputOutput = hashMapOf(
+                intArrayOf(0) to intArrayOf(0),
+                intArrayOf(1) to intArrayOf(1),
+                intArrayOf(1, 0) to intArrayOf(1, 0),
+                intArrayOf(0, 1) to intArrayOf(1, 0),
+                intArrayOf(1, 0, 0) to intArrayOf(1, 0, 0),
+                intArrayOf(0, 1, 0, 3, 12) to intArrayOf(1, 3, 12, 0, 0)
+        )
+
+        expectedInputOutput.forEach { (input, output) ->
+            var arr = input
+            solution.moveZeroes(arr)
+            assertArrayEquals(output, arr)
+        }
     }
 
 }
