90df43339e779634714738e948bcba52674bb553
Navid Rojiani <nrojiani@gmail.com>
2017-12-06 01:31:58 -0800

    434, 437

---
+++ src/main/kotlin/algorithms/easy/_434_number_of_segments_string/NumberOfSegmentsInString.kt
@@ -0,0 +1,10 @@
+package algorithms.easy._434_number_of_segments_string
+
+/**
+ * 434 - https://leetcode.com/problems/number-of-segments-in-a-string/description/
+ */
+class Solution {
+    fun countSegments(s: String): Int = s.split(" ")
+            .filterNot { it.isBlank() }
+            .size
+}
