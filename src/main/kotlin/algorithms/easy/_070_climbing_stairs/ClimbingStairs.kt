4686ca999e13c5845d48d6484d5d4ec1ae59599d
Navid Rojiani <nrojiani@gmail.com>
2018-07-18 01:05:04 -0400

    Climb Stairs

--- src/main/kotlin/algorithms/easy/_070_climbing_stairs/ClimbingStairs.kt
+++ src/main/kotlin/algorithms/easy/_070_climbing_stairs/ClimbingStairs.kt
@@ -3,42 +3,95 @@ package algorithms.easy._070_climbing_stairs
 /**
  * 70 - https://leetcode.com/problems/climbing-stairs/description/
  */
-class Solution {
+class NaiveSolution {
+
+
+    /**
+     * Time: O()
+     * Space: O(?)
+     */
+    fun climbStairs(n: Int): Int {
+        require(n > 0) { "n must be a positive integer" }
+
+        return when (n) {
+            in 1..2 -> n
+            else -> climbStairs(n - 1) + climbStairs(n - 2)
+        }
+    }
+}
+
+class MemoizedSolution {
+
+    private val cache = hashMapOf<Int, Int>()
+
     /**
      * Time: O(n)
      * Space: O(n)
      */
-    fun climbStairsMemoized(n: Int): Int {
-        val waysToClimb = mutableMapOf(
-                0 to 0,
-                1 to 1,
-                2 to 2
+    fun climbStairs(n: Int): Int {
+        require(n > 0) { "n must be a positive integer" }
+
+        return when (n) {
+            in 1..2 -> n
+            else -> {
+                if (!cache.containsKey(n)) {
+                    cache[n] = climbStairs(n - 1) + climbStairs(n - 2)
+                }
+                cache[n]!!
+            }
+        }
+    }
+}
+
+class TabulationSolution {
+
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun climbStairs(n: Int): Int {
+        require(n > 0) { "n must be a positive integer" }
+
+        val cache = hashMapOf(
+            1 to 1,
+            2 to 2
         )
 
-        (3..n).forEach { x ->
-            waysToClimb[x] = waysToClimb[x - 1]!! + waysToClimb[x - 2]!!
+        if (n in 1..2) return cache[n]!!
+
+        (3..n).forEach { i ->
+            cache[i] = cache[i - 1]!! + cache[i - 2]!!
         }
 
-        return waysToClimb[n]!!
+        return cache[n]!!
     }
+}
+
+class OptimalSolution {
 
     /**
      * Time: O(n)
      * Space: O(1)
      */
     fun climbStairs(n: Int): Int {
-        if (n in (0..2))
-            return n
-        var waysToClimbTwoLess = 1
-        var waysToClimbOneLess = 2
-        var waysToClimbX = 3
+        require(n > 0) { "n must be a positive integer" }
 
-        (3..n).forEach {
-            waysToClimbX = waysToClimbOneLess + waysToClimbTwoLess
-            waysToClimbTwoLess = waysToClimbOneLess
-            waysToClimbOneLess = waysToClimbX
+        return when (n) {
+            in 1..2 -> n
+            else -> {
+                var minus1 = 2  // f(2)
+                var minus2 = 1  // f(1)
+                var current = minus1 + minus2
+
+                (3..n).forEach {
+                    current = minus1 + minus2
+                    minus2 = minus1
+                    minus1 = current
+                }
+
+                current
+            }
         }
 
-        return waysToClimbX
     }
 }
--- src/test/kotlin/algorithms/easy/_070_climbing_stairs/ClimbingStairsTest.kt
+++ src/test/kotlin/algorithms/easy/_070_climbing_stairs/ClimbingStairsTest.kt
@@ -5,24 +5,45 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class ClimbingStairsTest {
-    private val solution = Solution()
+    private val naiveSolution = NaiveSolution()
+    private val memo = MemoizedSolution()
+    private val tabulation = TabulationSolution()
+    private val optimal = OptimalSolution()
 
     @Test
     fun climbStairs() {
-        assertEquals(0, solution.climbStairs(0))
-        assertEquals(1, solution.climbStairs(1))
-        assertEquals(2, solution.climbStairs(2))
-        assertEquals(3, solution.climbStairs(3))
-        assertEquals(5, solution.climbStairs(4))
+        assertEquals(1, naiveSolution.climbStairs(1))
+        assertEquals(2, naiveSolution.climbStairs(2))
+        assertEquals(3, naiveSolution.climbStairs(3))
+        assertEquals(5, naiveSolution.climbStairs(4))
+        assertEquals(34, naiveSolution.climbStairs(8))
     }
 
     @Test
     fun climbStairsMemoized() {
-        assertEquals(0, solution.climbStairsMemoized(0))
-        assertEquals(1, solution.climbStairsMemoized(1))
-        assertEquals(2, solution.climbStairsMemoized(2))
-        assertEquals(3, solution.climbStairsMemoized(3))
-        assertEquals(5, solution.climbStairsMemoized(4))
+        assertEquals(1, memo.climbStairs(1))
+        assertEquals(2, memo.climbStairs(2))
+        assertEquals(3, memo.climbStairs(3))
+        assertEquals(5, memo.climbStairs(4))
+        assertEquals(34, memo.climbStairs(8))
+    }
+
+    @Test
+    fun climbStairsTabulation() {
+        assertEquals(1, tabulation.climbStairs(1))
+        assertEquals(2, tabulation.climbStairs(2))
+        assertEquals(3, tabulation.climbStairs(3))
+        assertEquals(5, tabulation.climbStairs(4))
+        assertEquals(34, tabulation.climbStairs(8))
+    }
+
+    @Test
+    fun climbStairsOptimal() {
+        assertEquals(1, optimal.climbStairs(1))
+        assertEquals(2, optimal.climbStairs(2))
+        assertEquals(3, optimal.climbStairs(3))
+        assertEquals(5, optimal.climbStairs(4))
+        assertEquals(34, optimal.climbStairs(8))
     }
 
 }
