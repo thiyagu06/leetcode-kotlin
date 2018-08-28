134211daa85d73bf841bed48a8608805bdcb2f92
Navid Rojiani <nrojiani@gmail.com>
2018-08-28 19:25:34 -0400

    Sliding Window Maximum (H)

---
+++ src/test/kotlin/algorithms/hard/_239_sliding_window_maximum/SlidingWindowMaximumTest.kt
@@ -0,0 +1,25 @@
+package algorithms.hard._239_sliding_window_maximum
+
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
+class SlidingWindowMaximumTest {
+
+    private val solution = Solution()
+    private val followup = FollowUp()
+
+    @Test
+    fun maxSlidingWindow() {
+        assertArrayEquals(intArrayOf(3, 3, 5, 5, 6, 7),
+            solution.maxSlidingWindow(intArrayOf(1, 3, -1, -3, 5, 3, 6, 7), 3))
+        assertArrayEquals(intArrayOf(), solution.maxSlidingWindow(intArrayOf(), 0))
+    }
+
+    @Test
+    fun maxSlidingWindowOptimal() {
+        assertArrayEquals(intArrayOf(3, 3, 5, 5, 6, 7),
+            followup.maxSlidingWindow(intArrayOf(1, 3, -1, -3, 5, 3, 6, 7), 3))
+
+        assertArrayEquals(intArrayOf(), followup.maxSlidingWindow(intArrayOf(), 0))
+    }
+}
