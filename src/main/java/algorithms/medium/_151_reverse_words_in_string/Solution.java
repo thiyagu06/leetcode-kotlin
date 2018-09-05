0f187f8482f7b468deb6105b8aa2d3414c08f0ee
Navid Rojiani <nrojiani@gmail.com>
2018-09-04 22:07:11 -0400

    Reverse Words in String I

---
+++ src/main/java/algorithms/medium/_151_reverse_words_in_string/Solution.java
@@ -0,0 +1,16 @@
+package algorithms.medium._151_reverse_words_in_string;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public String reverseWords(String s) {
+        final String trimmed = s.trim();
+        final String[] split = trimmed.split(" ");
+        final List<String> words = Arrays.stream(split).filter(word -> !word.isEmpty()).collect(Collectors.toList());
+        Collections.reverse(words);
+        return words.stream().collect(Collectors.joining(" "));
+    }
+}
