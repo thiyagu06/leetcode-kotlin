d2109a7eb2e8d1ff1d72dd49c9e2c76a73c9f449
Navid Rojiani <nrojiani@gmail.com>
2018-09-07 21:57:13 -0400

    Bit Manipulation extensions

--- src/main/kotlin/extensions/math/Binary.kt
+++ src/main/kotlin/extensions/math/Binary.kt
@@ -21,5 +21,54 @@ fun Int.toBinaryString(bits: Int = 32): String = when {
     else -> java.lang.Integer.toBinaryString(this)
 }
 
+/**
+ * Converts a non-negative binary string to an integer.
+ * @exception  NumberFormatException If the binary string represents a negative number or can't be parsed
+ */
+fun String.parseNonNegativeBinaryString(): Int = Integer.parseInt(this, 2)
+
 fun Int.isOdd(): Boolean = (this and 1) == 1
 fun Int.isEven(): Boolean = (this and 1) == 0
+
+enum class BinaryValue { ZERO, ONE }
+
+/**
+ * @return the Int produced by setting the last bit to 0 or 1
+ */
+fun Int.withLastBitSetTo(value: BinaryValue): Int = when (value) {
+    BinaryValue.ZERO -> (this or 1) - 1
+    BinaryValue.ONE -> this or 1
+}
+
+/**
+ * TODO keep & remove withLastBitSetTo or delete
+ * @return the Int produced by setting the last bit (to 1)
+ */
+fun Int.withLastBitSet(): Int = this or 1
+
+/**
+ * TODO keep & remove withLastBitSetTo or delete
+ * @return the Int produced by unsetting the last bit (i.e., making it 0)
+ */
+fun Int.withLastBitUnset(): Int = (this or 1) - 1
+
+
+val Int.isPowerOfTwo: Boolean get() = this > 0 && (this and (this - 1)) == 0
+
+/**
+ * @param k the kth least significant digit, where k = 0 is the least significant bit.
+ */
+fun Int.hasKthBitSet(k: Int): Boolean {
+    require(k in 0..31) { "k must be in range 0..31" }
+    return (this and (1 shl k)) != 0
+}
+
+/**
+ * @param k the kth least significant digit, where k = 0 is the least significant bit.
+ */
+fun Int.valueOfKthBit(k: Int): Int = if (hasKthBitSet(k)) 1 else 0
+
+
+// TODO
+// withKthBit(Set/Unset)
+// toggleKthBit()
--- src/test/kotlin/extensions/math/BinaryTest.kt
+++ src/test/kotlin/extensions/math/BinaryTest.kt
@@ -1,8 +1,11 @@
 package extensions.math
 
-import junit.framework.Assert.assertEquals
 import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.math.pow
 
 class BinaryTest {
 
@@ -16,6 +19,24 @@ class BinaryTest {
     fun isOdd() {
         setOf(-9, -3, -1, 1, 3, 7, 21, 101, 127).forEach { Assert.assertTrue(it.isOdd()) }
         setOf(-10, -4, 0, 2, 4, 6, 20, 100, 128).forEach { Assert.assertFalse(it.isOdd()) }
+    }
+
+    @Test
+    fun isPowerOfTwo() {
+        val powersOfTwo = (0..20).map { n ->
+            2.0.pow(n).toInt()
+        }
+
+        (0..1000).forEach { n ->
+            if (n in powersOfTwo) {
+                assertTrue(n.isPowerOfTwo)
+            } else {
+                assertFalse(n.isPowerOfTwo)
+            }
+        }
+
+        assertFalse((-1).isPowerOfTwo)
+        assertFalse((-8).isPowerOfTwo)
     }
 
     @Test
@@ -45,5 +66,122 @@ class BinaryTest {
         // Overflow
         assertEquals(Int.MIN_VALUE.toBinaryString(), (Int.MAX_VALUE + 1).toBinaryString())
         assertEquals(Int.MAX_VALUE.toBinaryString(), (Int.MIN_VALUE - 1).toBinaryString())
+    }
+
+    @Test
+    fun parseNonNegativeBinaryString() {
+        assertEquals(0, "0".parseNonNegativeBinaryString())
+        assertEquals(1, "1".parseNonNegativeBinaryString())
+        assertEquals(1, "0001".parseNonNegativeBinaryString())
+        assertEquals(1, "00000000000000000000000000000001".parseNonNegativeBinaryString())
+        assertEquals(974321, "00000000000011101101110111110001".parseNonNegativeBinaryString())
+        assertEquals(Int.MAX_VALUE, "01111111111111111111111111111111".parseNonNegativeBinaryString())
+    }
+
+    @Test(expected = NumberFormatException::class)
+    fun `parseNonNegativeBinaryString throws exception when called on negative Int MIN_VALUE`() {
+        "10000000000000000000000000000001".parseNonNegativeBinaryString()
+    }
+
+    @Test(expected = NumberFormatException::class)
+    fun `parseNonNegativeBinaryString throws exception when called on -1`() {
+        "11111111111111111111111111111111".parseNonNegativeBinaryString()
+    }
+
+    @Test
+    fun withLastBitSetTo() {
+        assertEquals(0, 0.withLastBitSetTo(BinaryValue.ZERO))
+        assertEquals(1, 0.withLastBitSetTo(BinaryValue.ONE))
+
+        assertEquals(0, 1.withLastBitSetTo(BinaryValue.ZERO))
+        assertEquals(1, 1.withLastBitSetTo(BinaryValue.ONE))
+
+        assertEquals(10, 10.withLastBitSetTo(BinaryValue.ZERO))
+        assertEquals(11, 10.withLastBitSetTo(BinaryValue.ONE))
+
+        assertEquals(6, 7.withLastBitSetTo(BinaryValue.ZERO))
+        assertEquals(7, 7.withLastBitSetTo(BinaryValue.ONE))
+
+        assertEquals(-2, (-1).withLastBitSetTo(BinaryValue.ZERO))
+        assertEquals(-1, (-1).withLastBitSetTo(BinaryValue.ONE))
+
+        assertEquals(-8, (-7).withLastBitSetTo(BinaryValue.ZERO))
+        assertEquals(-7, (-7).withLastBitSetTo(BinaryValue.ONE))
+    }
+
+    @Test
+    fun withLastBitSet() {
+        assertEquals(1, 0.withLastBitSet())
+        assertEquals(1, 1.withLastBitSet())
+        assertEquals(11, 10.withLastBitSet())
+        assertEquals(7, 7.withLastBitSet())
+        assertEquals(-1, (-1).withLastBitSet())
+        assertEquals(-7, (-7).withLastBitSet())
+    }
+
+    @Test
+    fun withLastBitUnset() {
+        assertEquals(0, 0.withLastBitUnset())
+        assertEquals(0, 1.withLastBitUnset())
+        assertEquals(10, 10.withLastBitUnset())
+        assertEquals(6, 7.withLastBitUnset())
+        assertEquals(-2, (-1).withLastBitUnset())
+        assertEquals(-8, (-7).withLastBitUnset())
+    }
+
+    @Test
+    fun hasKthBitSet() {
+        (0..3).forEach { k ->
+            assertTrue(15.hasKthBitSet(k))
+        }
+        (4..31).forEach { k ->
+            assertFalse(15.hasKthBitSet(k))
+        }
+
+        (0..31).forEach { k ->
+            assertFalse(0.hasKthBitSet(k))
+        }
+
+        (0..31).forEach { k ->
+            assertTrue((-1).hasKthBitSet(k))
+        }
+
+        assertFalse(10.hasKthBitSet(0))
+        assertTrue(10.hasKthBitSet(1))
+        assertFalse(10.hasKthBitSet(2))
+        assertTrue(10.hasKthBitSet(3))
+
+        assertFalse((-5).hasKthBitSet(2))
+        assertTrue((-5).hasKthBitSet(0))
+        assertTrue((-5).hasKthBitSet(1))
+        assertTrue((-5).hasKthBitSet(3))
+    }
+
+    @Test
+    fun valueOfKthBit() {
+        (0..3).forEach { k ->
+            assertEquals(1, 15.valueOfKthBit(k))
+        }
+        (4..31).forEach { k ->
+            assertEquals(0, 15.valueOfKthBit(k))
+        }
+
+        (0..31).forEach { k ->
+            assertEquals(0, 0.valueOfKthBit(k))
+        }
+
+        (0..31).forEach { k ->
+            assertEquals(1, (-1).valueOfKthBit(k))
+        }
+
+        assertEquals(0, 10.valueOfKthBit(0))
+        assertEquals(1, 10.valueOfKthBit(1))
+        assertEquals(0, 10.valueOfKthBit(2))
+        assertEquals(1, 10.valueOfKthBit(3))
+
+        assertEquals(0, (-5).valueOfKthBit(2))
+        assertEquals(1, (-5).valueOfKthBit(0))
+        assertEquals(1, (-5).valueOfKthBit(1))
+        assertEquals(1, (-5).valueOfKthBit(3))
     }
 }
