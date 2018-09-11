9d491d7f779bd3facd5d06480f752ad1d6512931
Navid Rojiani <nrojiani@gmail.com>
2018-09-10 20:13:19 -0400

    Decode String (#394)

--- src/main/kotlin/leetcode/medium/_394_decode_string/DecodeString.kt
+++ src/main/kotlin/leetcode/medium/_394_decode_string/DecodeString.kt
@@ -1,67 +1,159 @@
 package leetcode.medium._394_decode_string
 
 import extensions.chars.numericValue
-import java.util.Stack
 
 /**
  * 394 - https://leetcode.com/problems/decode-string
  */
 class Solution {
+
+    /**
+     * Time: O(n)  ?
+     * Space: O(n) ?
+     */
+    fun decodeString(s: String): String = when {
+        s.isEmpty() -> s
+        else -> {
+            val k = parseFirstK(s)
+
+            // when k = 1, can either be "ef", or "1[ef]"
+            val encodedStrStartIdx =
+                if (s[0].isLetter()) 0 else k.toString().length
+
+            // "3[a]2[b4[F]c]": k = 3, encodedStr = "a", remaining = "2[b4[F]c]"
+            val (encodedString, remaining) = readFirstEncodedString(s.substring(encodedStrStartIdx))
+
+            if (encodedString.isDecoded()) {
+                encodedString.repeat(k) + decodeString(remaining)
+            } else {
+                decodeString(encodedString).repeat(k) + decodeString(remaining)
+            }
+        }
+    }
+
+    // "ef" -> 1
+    // "3[a]" -> 3
+    internal fun parseFirstK(s: String): Int = when {
+        s.first().isLetter() -> 1
+        else -> s.takeWhile { it.isDigit() }.toInt()
+    }
+
+    /**
+     * Reads the first encoded string, which will start at the beginning of the string (k already parsed),
+     * @return the first encoded string, and the remainder of [s].
+     */
+    internal fun readFirstEncodedString(s: String): Pair<String, String> {
+        when {
+            // no braces
+            s.first().isLetter() -> {
+                val encoded = s.takeWhile { it.isLetter() }
+                val remainder = s.substring(encoded.length)
+                return Pair(encoded, remainder)
+            }
+
+            // braces
+            else -> {
+                var count = 0
+                var encoded = ""
+                s.forEach { c ->
+
+                    if (c == '[') count++
+                    if (c == ']') count--
+
+                    when (c to count) {
+                        '[' to 1 -> return@forEach
+                        ']' to 0 -> return Pair(encoded, s.substring(encoded.length + 2))
+                        else -> encoded += c
+                    }
+
+                }
+                return Pair(encoded, s.substring(encoded.length + 2))
+            }
+        }
+    }
+
+    /**
+     * Time: O(n)
+     * Space: O(1)
+     */
+    private fun String.isDecoded(): Boolean = all { it.isLetter() }
+}
+
+class SolutionTwo {
+
+    /**
+     * Time: O(?)
+     * Space: O(?)
+     */
     fun decodeString(s: String): String {
         var result = s
-        while ('[' in result) {
+        while (!result.isDecoded()) {
             result = denestInput(result)
         }
         return result
     }
 
     internal fun denestInput(input: String): String {
-        var result = ""
-        var currentStr = ""
-        val stack = Stack<Char>()
+        val segments = arrayListOf<String>()
+        var currentEncoding = ""
+        var count = 0
         var k = 1
         var kDigits = ""
 
         input.forEach { c ->
-            when (c) {
-                '[' -> {
-                    stack.push('[')
-                    if (stack.size > 1) {
-                        currentStr += '['
+            when {
+                c == '[' && count == 0 -> {
+                    if (currentEncoding.isNotEmpty()) {
+                        segments.add(currentEncoding)
+                        currentEncoding = ""
                     }
+                    count++
+                }
+                c == '[' && count > 0 -> {
+                    currentEncoding += '['
+                    count++
                 }
 
-                ']' -> {
-                    stack.pop()
-                    if (stack.isNotEmpty()) {
-                        currentStr += ']'
+                c == ']' -> {
+                    count--
+                    if (count > 0) {
+                        currentEncoding += ']'
                     } else {
-                        result += currentStr.repeat(k)
-                        currentStr = ""
+                        segments.add(currentEncoding.repeat(k))
+                        currentEncoding = ""
                         k = 1
                         kDigits = ""
                     }
                 }
 
-                in '0'..'9' -> {
-                    if (stack.isEmpty()) {
-                        kDigits += c.numericValue()
-                        k = kDigits.toInt()
-                    } else {
-                        currentStr += c
-                    }
+                c == ']' && count > 0 -> {
+                    count--
+                    currentEncoding += ']'
                 }
 
-                in 'a'..'z' -> {
-                    if (stack.isEmpty()) {  // un-nested input, like ef in "3[a]ef"
-                        result += c
-                    } else {
-                        currentStr += c
-                    }
+                c == ']' && count == 0 -> {
+                    count--
+                    segments.add(currentEncoding.repeat(k))
+                    currentEncoding = ""
+                    k = 1
+                    kDigits = ""
                 }
+
+                c.isDigit() && count == 0 -> {
+                    kDigits += c.numericValue()
+                    k = kDigits.toInt()
+                }
+
+                else -> currentEncoding += c
             }
         }
 
-        return result
+        if (currentEncoding.isNotEmpty())
+            segments.add(currentEncoding)
+
+        return segments.joinToString("")
     }
+
+    private fun String.isDecoded(): Boolean = all { it.isLetter() }
 }
+
--- src/test/kotlin/leetcode/medium/_394_decode_string/DecodeStringTest.kt
+++ src/test/kotlin/leetcode/medium/_394_decode_string/DecodeStringTest.kt
@@ -6,25 +6,69 @@ import org.junit.Test
 class DecodeStringTest {
 
     private val solution = Solution()
+    private val solutionTwo = SolutionTwo()
 
     @Test
     fun decodeString() {
+        assertEquals("cc", solution.decodeString("2[c]"))
         assertEquals("aaabcbc", solution.decodeString("3[a]2[bc]"))
         assertEquals("abcabccdcdcdef", solution.decodeString("2[abc]3[cd]ef"))
-        assertEquals("accaccacc", solution.decodeString("3[a2[c]]"))
         assertEquals("codecodecodecodecodecodecodecodecodecode", solution.decodeString("10[code]"))
+        assertEquals("accaccacc", solution.decodeString("3[a2[c]]"))
+        assertEquals("aaabFFFFcbFFFFc", solution.decodeString("3[a]2[b4[F]c]"))
+        assertEquals("bFFFFcbFFFFcaaa", solution.decodeString("2[b4[F]c]3[a]"))
+        assertEquals("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef", solution.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
+    }
+
+    @Test
+    fun parseFirstK() {
+        assertEquals(1, solution.parseFirstK("ef"))
+        assertEquals(1, solution.parseFirstK("1[f]"))
+        assertEquals(3, solution.parseFirstK("3[a]"))
+        assertEquals(3, solution.parseFirstK("3[abc]"))
+        assertEquals(3, solution.parseFirstK("3[a2[bc]]"))
+        assertEquals(10, solution.parseFirstK("10[a]"))
+        assertEquals(9999, solution.parseFirstK("9999[a]"))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun parseFirstKInvalid() {
+        solution.parseFirstK("[abc]")
+    }
+
+    @Test
+    fun readFirstEncodedString() {
+        assertEquals(Pair("ef", ""), solution.readFirstEncodedString("ef"))
+        assertEquals(Pair("ef", "[a2[c]]"), solution.readFirstEncodedString("ef[a2[c]]"))
+        assertEquals(Pair("a", ""), solution.readFirstEncodedString("[a]"))
+        assertEquals(Pair("abc", ""), solution.readFirstEncodedString("[abc]"))
+        assertEquals(Pair("a2[c]", ""), solution.readFirstEncodedString("[a2[c]]"))
+        assertEquals(Pair("b4[F]c", ""), solution.readFirstEncodedString("[b4[F]c]"))
+        assertEquals(Pair("b4[F]c", "[a2[c]]"), solution.readFirstEncodedString("[b4[F]c][a2[c]]"))
+        assertEquals(Pair("b", "4[F]c"), solution.readFirstEncodedString("b4[F]c"))
+        assertEquals(Pair("a", "2[c]a2[c]a2[c]"), solution.readFirstEncodedString("a2[c]a2[c]a2[c]"))
+        assertEquals(Pair("abc", "3[cd]ef"), solution.readFirstEncodedString("[abc]3[cd]ef"))
+        assertEquals(Pair("a2[c]", ""), solution.readFirstEncodedString("[a2[c]]"))
+    }
+
+    @Test
+    fun decodeString2() {
+        assertEquals("cc", solutionTwo.decodeString("2[c]"))
+        assertEquals("aaabcbc", solutionTwo.decodeString("3[a]2[bc]"))
+        assertEquals("abcabccdcdcdef", solutionTwo.decodeString("2[abc]3[cd]ef"))
+        assertEquals("codecodecodecodecodecodecodecodecodecode", solutionTwo.decodeString("10[code]"))
+        assertEquals("accaccacc", solutionTwo.decodeString("3[a2[c]]"))
+        assertEquals("aaabFFFFcbFFFFc", solutionTwo.decodeString("3[a]2[b4[F]c]"))
+        assertEquals("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef", solutionTwo.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
     }
 
     @Test
     fun denestInput() {
-        assertEquals("aaa", solution.denestInput("3[a]"))
-        assertEquals("aaabcbc", solution.denestInput("3[a]2[bc]"))
-        assertEquals("ef", solution.denestInput("ef"))
-        assertEquals("aaaef", solution.denestInput("3[a]ef"))
-        assertEquals("abcabccdcdcdef", solution.denestInput("2[abc]3[cd]ef"))
-
-        // Recursively nested:
-        assertEquals("a2[c]a2[c]a2[c]", solution.denestInput("3[a2[c]]"))
-        assertEquals("accaccacc", solution.denestInput("a2[c]a2[c]a2[c]"))
+        assertEquals("aaa", solutionTwo.denestInput("3[a]"))
+        assertEquals("aaabcbc", solutionTwo.denestInput("3[a]2[bc]"))
+        assertEquals("ef", solutionTwo.denestInput("ef"))
+        assertEquals("aaaef", solutionTwo.denestInput("3[a]ef"))
+        assertEquals("abcabccdcdcdef", solutionTwo.denestInput("2[abc]3[cd]ef"))
+        assertEquals("aaab4[F]cb4[F]c", solutionTwo.denestInput("3[a]2[b4[F]c]"))
     }
 }
