2b292c84d12a5cdfd8220ef8c177a5bf1d3552b4
Navid Rojiani <nrojiani@gmail.com>
2018-09-18 14:28:01 -0400

    Review: Is Symmetric Binary Tree (add iterative soln)

--- src/main/kotlin/leetcode/easy/_101_is_symmetric_tree/IsSymmetricTree.kt
+++ src/main/kotlin/leetcode/easy/_101_is_symmetric_tree/IsSymmetricTree.kt
@@ -1,6 +1,8 @@
 package leetcode.easy._101_is_symmetric_tree
 
 import datastructures.tree.TreeNode
+import java.util.LinkedList
+import java.util.Queue
 
 /**
  * 101 - https://leetcode.com/problems/symmetric-tree/description/
@@ -13,11 +15,43 @@ class Solution {
     fun isSymmetric(root: TreeNode?): Boolean =
         (root == null) || areMirrorNodes(root.left, root.right)
 
-    private fun areMirrorNodes(l: TreeNode?, r: TreeNode?): Boolean = when {
-        l == null && r == null -> true
-        (l == null && r != null) || (l != null && r == null) -> false
-        else -> (l?.`val` == r?.`val`)
-                && areMirrorNodes(l?.left, r?.right)
-                && areMirrorNodes(l?.right, r?.left)
+    private fun areMirrorNodes(p: TreeNode?, q: TreeNode?): Boolean = when {
+        p == null || q == null -> p == q
+        p.`val` != q.`val` -> false
+        else -> areMirrorNodes(p.left, q.right) && areMirrorNodes(p.right, q.left)
+    }
+}
+
+class IterativeSolution {
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun isSymmetric(root: TreeNode?): Boolean {
+        root ?: return true
+
+        val queue: Queue<TreeNode> = LinkedList<TreeNode>()
+        queue.add(root.left)
+        queue.add(root.right)
+
+        while (queue.size >= 2) {
+            val (left, right) = (queue.poll() to queue.poll())
+
+            if (left == null && right == null)
+                continue
+
+            when {
+                (left == null) xor (right == null) -> return false
+                left.`val` != right.`val` -> return false
+                else -> {
+                    queue.add(left.left)
+                    queue.add(right.right)
+                    queue.add(left.right)
+                    queue.add(right.left)
+                }
+            }
+        }
+
+        return true
     }
 }
--- src/test/kotlin/leetcode/easy/_101_is_symmetric_tree/IsSymmetricTreeTest.kt
+++ src/test/kotlin/leetcode/easy/_101_is_symmetric_tree/IsSymmetricTreeTest.kt
@@ -8,6 +8,7 @@ import org.junit.Assert.*
 class IsSymmetricTreeTest {
 
     private val solution = Solution()
+    private val iterativeSolution = IterativeSolution()
 
     @Test
     fun isSymmetric() {
@@ -46,5 +47,49 @@ class IsSymmetricTreeTest {
         //   / \ / \
         //  3  4 3  4
         assertFalse(solution.isSymmetric(buildTree(1, 2, 2, 3, 4, 3, 4)))
+
+        assertFalse(solution.isSymmetric(buildTree(9, -42, -42, null, 76, 76, null, null, 13, null, 13)))
+    }
+
+
+    @Test
+    fun isSymmetricIterative() {
+        assertTrue(iterativeSolution.isSymmetric(null))
+        assertTrue(iterativeSolution.isSymmetric(buildTree(1)))
+        assertTrue(iterativeSolution.isSymmetric(buildTree(1, 2, 2)))
+
+        //      1
+        //     / \
+        //    2   2
+        //   / \ / \
+        //  3  4 4  3
+        assertTrue(iterativeSolution.isSymmetric(buildTree(1, 2, 2, 3, 4, 4, 3)))
+
+        //          1
+        //      /       \
+        //     2         2
+        //    / \       / \
+        //   3   4     4   3
+        //  /\   /\   /\   /\
+        // 5  6 7  8  8 7 6  5
+        assertTrue(iterativeSolution.isSymmetric(buildTree(1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5)))
+
+        assertFalse(iterativeSolution.isSymmetric(buildTree(1, 2)))
+
+        //    1
+        //   / \
+        //  2   2
+        //   \   \
+        //    3    3
+        assertFalse(iterativeSolution.isSymmetric(buildTree(1, 2, 2, null, 3, null, 3)))
+
+        //      1
+        //     / \
+        //    2   2
+        //   / \ / \
+        //  3  4 3  4
+        assertFalse(iterativeSolution.isSymmetric(buildTree(1, 2, 2, 3, 4, 3, 4)))
+
+        assertFalse(iterativeSolution.isSymmetric(buildTree(9, -42, -42, null, 76, 76, null, null, 13, null, 13)))
     }
 }
