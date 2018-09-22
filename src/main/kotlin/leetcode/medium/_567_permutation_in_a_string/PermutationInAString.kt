a3ea9d7a5dfdf2345ec6520eaa87f4b1d1dd1abe
Navid Rojiani <nrojiani@gmail.com>
2018-09-21 21:00:06 -0400

    fix Permutation in a String (Sliding Window/Substring)

--- src/main/kotlin/leetcode/medium/_567_permutation_in_a_string/PermutationInAString.kt
+++ src/main/kotlin/leetcode/medium/_567_permutation_in_a_string/PermutationInAString.kt
@@ -4,139 +4,156 @@ import extensions.strings.characterFrequencies
 
 class Solution {
     /**
-     * Time: O(n)
-     * Space: O(n)
+     * Time: O(?)
+     * Space: O(?)
      */
     fun checkInclusion(s1: String, s2: String): Boolean {
         var count = s1.length
         val n = s2.length
         var hash = s1.characterFrequencies().toMutableMap()
-        var l = 0
-        var r = 0
+        var leftIdx = 0
+        var rightIdx = 0
 
-        println("s1: $s1")
-        println("s2: $s2")
 
-        while (l < n && r < n && count > 0) {
-//            if (hash.values.all { it == 0 }) return true
-
-            val (charL, charR) = (s2[l] to s2[r])
-            println("\n(l, r) = ($l, $r), (charL, charR) = ($charL, $charR)")
-            println("s2[l:r] = ${s2.substring(l..r)}")
-            println("hash: $hash")
+        while (count > 0 && leftIdx < n && rightIdx < n) {
+            val (charL, charR) = (s2[leftIdx] to s2[rightIdx])
             when {
-                // charR is in s1
-                hash.containsKey(charR) -> {
-                    println("hash.contains($charR) -> T")
-                    // and is not an overuse
-                    if (hash[charR]!! > 0) {
-                        println("not an overuse of $charR (hash[charR] == ${hash[charR]})")
-                        hash[charR] = hash[charR]!! - 1
-                        count--
-                        println("count-- -> now $count")
-                        r++     // widen window (l stays at same index)
-                    } else {
-                        println("overuse of $charR (hash[charR] == ${hash[charR]})")
-                        // charR is overused. window contains more of the char than s1 does.
-                        // evict left
-                        if (hash.containsKey(charL)) {
-                            hash[charL] = hash[charL]!! + 1
-                            println("count++ -> now $count")
-                            count++
-                        }
-                        l++     // r will be rechecked
-                    }
+                // charR is in s1, and is not 'overused' in current window.
+                // modify hash & count, and expand window to the right.
+                charR in hash && hash[charR]!! > 0 -> {
+                    count--
+                    hash[charR] = hash[charR]!! - 1
+                    rightIdx++
                 }
-                // charR not in s1
+                // charR in hash, but we've already found the necessary number of occurrences.
+                // Evict charL and shorten the window.
+                // If charL was in hash, modify hash & count since its no longer in the window.
+                // Don't move R pointer up yet - charR will be rechecked on next iteration.
+                charL in hash -> {  // could replace with leftIdx != rightIdx. If charL in hash, the window size is > 1
+                    count++
+                    hash[charL] = hash[charL]!! + 1
+                    leftIdx++
+                }
+
+                // charR not in s1, and evicted char not in s1.
+                // Evict charL and shorten the window.
+                // leftIdx = rightIdx in this case (window size is 1 letter).
+                // Slide both indices up to next letter.
                 else -> {
-                    println("hash.contains($charR) -> F")
-                    // evict left
-                    if (hash.containsKey(charL)) {  // charL was in s1.
-                        println("hash.contains($charL) -> T. hash[$charL]++, count++")
-                        hash[charL] = hash[charL]!! + 1
-                        println("count++ -> now $count")
-                        count++
-                    }
-                    r++
-                    l++
+                    leftIdx++
+                    rightIdx++
                 }
             }
-            println("(END LOOP) -> hash: $hash, count: $count")
         }
-        println("\n(END) -> hash: $hash, count: $count")
+        return count == 0
+    }
+}
+
+class SolutionTwo {
+    /**
+     * Same as the solution above, uses if instead of when
+     *
+     * Time: O(?)
+     * Space: O(?)
+     */
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        var count = s1.length
+        val n = s2.length
+        var hash = s1.characterFrequencies().toMutableMap()
+        var leftIdx = 0
+        var rightIdx = 0
+
+        var loops = 0
+        while (count > 0 && leftIdx < n && rightIdx < n) {
+            val (charL, charR) = (s2[leftIdx] to s2[rightIdx])
+            // charR is in s1, and is not "overused" in current window.
+            // modify hash & count, and expand window to the right.
+            if (charR in hash && hash[charR]!! > 0) {
+                count--
+                hash[charR] = hash[charR]!! - 1
+                rightIdx++
+            } else {
+                // Two Possibilities:
+                // 1. charR isn't in hash (i.e., not in s1)
+                // 2. charR is in hash, but overused (hash[charR] <= 0)
+                // For either, evict charL and shorten window (leftIdx++)
+
+                if (charL in hash) {
+                    // If charL was in hash, modify hash & count since its no longer in the window.
+                    count++
+                    hash[charL] = hash[charL]!! + 1
+                } else {
+                    rightIdx++     // move to next r only if charL not in hash (window size is 1)
+                }
+                leftIdx++           // evict charL & shorten the window.
+            }
+            loops++
+        }
 
         return count == 0
     }
 }
 
-
-class SolutionTwo {
+class SolutionThree {
     /**
-     * Time: O(n)
-     * Space: O(n)
+     * Approach 6:
+     * https://leetcode.com/problems/permutation-in-string/solution/
+     *
+     * Time: O(l1 + (l2 - l1))
+     * Space: O(1)
      */
     fun checkInclusion(s1: String, s2: String): Boolean {
-        var count = s1.length
-        val n = s2.length
-        var hash = s1.characterFrequencies().toMutableMap()
-        var l = 0
-        var r = 0
+        val l1 = s1.length
+        val l2 = s2.length
 
-        println("s1: $s1")
-        println("s2: $s2")
+        var loops = 0
 
-        while (l < n && r < n && count > 0) {
-            val (charL, charR) = (s2[l] to s2[r])
-            println("\n(l, r) = ($l, $r), (charL, charR) = ($charL, $charR)")
-            println("s2[l:r] = ${s2.substring(l..r)}")
-            println("hash: $hash")
-            when {
-                // charR is in s1
-                hash.containsKey(charR) -> {
-                    println("hash.contains($charR) -> T")
-                    // and is not an overuse
-                    if (hash[charR]!! > 0) {
-                        println("not an overuse of $charR (hash[charR] == ${hash[charR]})")
-                        hash[charR] = hash[charR]!! - 1
-                        count--
-                        r++     // widen window (l stays at same index)
-                    } else {
-                        println("overuse of $charR (hash[charR] == ${hash[charR]})")
-                        // charR is overused. window contains more of the char than s1 does.
-                        // evict left
-                        if (hash.containsKey(charL)) {
-                            hash[charL] = hash[charL]!! + 1
-                            count++
-                        }
-                        l++     // r will be rechecked
-                    }
-                }
-                // charR not in s1
-                else -> {
-                    println("hash.contains($charR) -> F")
-                    // evict left
-                    if (hash.containsKey(charL)) {  // charL was in s1.
-                        println("hash.contains($charL) -> T. hash[$charL]++, count++")
-                        hash[charL] = hash[charL]!! + 1
-                    }
-                    count++
-                    r++
-                    // we know the rest of the window can't be used since the last isn't in s1
-                    // evict all in [l, r)
-                    (l until r).forEach { i ->
-                        if (hash.containsKey(s2[i])) {
-                            hash[s2[i]] = hash[s2[i]]!! + 1
-                        }
-                    }
-                    l = r
-                    count = s1.length
-                    println("(AFTER l->r EVICTIONS:) -> hash: $hash, count: $count")
-                }
-            }
-            println("(END LOOP) -> hash: $hash, count: $count")
+        if (l1 > l2) return false
+
+        val s1Map = IntArray(26)
+        val s2Map = IntArray(26)
+
+        (0..s1.lastIndex).forEach { i ->
+            s1Map[s1[i].alphaIndex]++
+            s2Map[s2[i].alphaIndex]++
+            loops++
         }
-        println("\n\n(END) -> hash: $hash, count: $count")
 
-        return count == 0
+        var count = 0
+
+        (0 until 26).forEach { i ->
+            if (s1Map[i] == s2Map[i]) {
+                count++
+            }
+            loops++
+        }
+
+        (0 until (l2 - l1)).forEach { i ->
+            // int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            val r = s2[i + l1].alphaIndex
+            val l = s2[i].alphaIndex
+
+            if (count == 26) {
+                return true
+            }
+
+            s2Map[r]++
+            if (s2Map[r] == s1Map[r])
+                count++
+            else if (s2Map[r] == s1Map[r] + 1)
+                count--
+
+            s2Map[l]--
+            if (s2Map[l] == s1Map[l])
+                count++
+            else if (s2Map[l] == s1Map[l] - 1)
+                count--
+
+            loops++
+        }
+
+        return count == 26
     }
+
+    val Char.alphaIndex get() = this - 'a'
 }
--- src/test/kotlin/leetcode/medium/_567_permutation_in_a_string/PermutationInAStringTest.kt
+++ src/test/kotlin/leetcode/medium/_567_permutation_in_a_string/PermutationInAStringTest.kt
@@ -8,6 +8,7 @@ class PermutationInAStringTest {
 
     private val solution = Solution()
     private val solution2 = SolutionTwo()
+    private val solution3 = SolutionThree()
     private val longS2 =
         "lcqhdarsfkqymriptcqrpfugsymeoambboebqrtgesytmyudzjnzmscgqpimvadeevwbtxewzccwqfxsshbwtjzasyvwujxfzcpmocyxmdhsyqogwyissjqbkecbqaweiqhhgcbvhuwfmhatdfidqeexuicsljbxempkpcxbattiikmnehqmblbvahgetyuabowbieiekihbdygpeboewbqlikxdxvnmvihmhmzzodkqagvrvyxyyjdcnroyvfzggadihpqtrtrihigysexvcyehkdyellqbxzjwubynduenjwaxobcokfnhclqvuhvcafsdygiqgmdmavobulleslqcfyghgeodfxsitawupalbudwehqzzdczxyhyweivuoeqgdurotkbbsolcsnkhqakpppdgklrgszdwnpkysbqczkjvwwdezbrmrwyapocwulmllnezndrmyyalyvvthjeizrmrccvqnfxrrwcsjilnlznohgwthbwnguqkloqnxwudkeeosimjihasknogkxafsgnvgrlymvhrubnekdmdgumgtlwhskyqsmvmsynsjadsghjhidbugtxhfrpzkhjwtqarwjyowdkjmpwnjfsznxokxhdjjxdqycbjqywipfcoxyzmwmemnihzcmdadegxzxkxnworbdjfmtyiwowfddmfmkxgauffznfgadsvfkbsfwpuhcjxtzfmwsllyglclaaheddqkglshspemwmymyexknqenbyzaxbecbzhteuysdcvxwzyommsmitpxnqdwnhxtitrxtfhfyddqsmqszmovivbiqknfdvqkioovsxbcouxpcqygsltosewaomqhkqlercbdxidazvddrlwbhwgqetjvusxynwljtwoluegezwumsvfocuyqxnrjkqqxfdkcueylgakvpqecgwlkmttdjxkgbmvbxyjsbtshudbvvsneiwbhzvpulsjtxkgbljnryzqmgkrjbmcqaxfstedhyyejdrgeoumzhjilrmemrrkfogbiwjifosipawkhbxruzwkgpbwvkxnrtkktuozwrlgdcqnoezzwqdgutsykwaizvgoccmnkujioxnoffhjpssqezpthqikhbckbyimozhcxszoqoknxmvkzmcigiwjplxjyssiauwgnvrgvokaxjvibbhodcxejzejobqeeppxyhvmozjprvrcodalyojuberheagstturwtvkpkkerjbiivuqmwjehuisjmciopnkjanghmfemxoawjzkjgiqsmoutxzuzguvfmobmxfhwzgchvvhmoudjmmxcwkjvezbvyqznpkaontyoepfabphrxrzdzldnwswbljdrervcmzgmqfsisxxicxxocgbvrciboavalvksmrwwfhjcbdsubewpbxqqpxnplrnlvbeuetesxfezuhymaalkjlahnvqwiruouafpxawrmhbmqfofpgdgckasfhjyrsknfpgnlfuxhzhoaoqdynfv"
 
@@ -24,16 +25,17 @@ class PermutationInAStringTest {
         assertTrue(solution.checkInclusion(s1 = "xy", s2 = "axbcdefyx"))
         assertFalse(solution.checkInclusion(s1 = "xxxyyy", s2 = "axxyxybc"))
         assertTrue(solution.checkInclusion(s1 = "xxxyyy", s2 = "axxyxyybc"))
+        assertTrue(solution.checkInclusion(s1 = "adc", s2 = "dcda"))
     }
 
     @Test
-    fun checkInclusionAlt() {
-//        assertTrue(
-//            solution.checkInclusion(
-//                s1 = "trinitrophenylmethylnitramine",
-//                s2 = "trinitrophenylmethylnitramine"
-//            )
-//        )
+    fun checkInclusionLong() {
+        assertTrue(
+            solution.checkInclusion(
+                s1 = "trinitrophenylmethylnitramine",
+                s2 = "trinitrophenylmethylnitramine"
+            )
+        )
         assertTrue(
             solution.checkInclusion(
                 s1 = "trinitrophenylmethylnitramine",
@@ -43,7 +45,7 @@ class PermutationInAStringTest {
     }
 
     @Test
-    fun checkInclusionLarge() {
+    fun checkInclusionVeryLong() {
         assertFalse(
             solution.checkInclusion(
                 s1 = "zftgfmnoxmfkwufwadd",
@@ -65,11 +67,12 @@ class PermutationInAStringTest {
         assertTrue(solution2.checkInclusion(s1 = "xy", s2 = "axbcdefyx"))
         assertFalse(solution2.checkInclusion(s1 = "xxxyyy", s2 = "axxyxybc"))
         assertTrue(solution2.checkInclusion(s1 = "xxxyyy", s2 = "axxyxyybc"))
+        assertTrue(solution2.checkInclusion(s1 = "adc", s2 = "dcda"))
     }
 
 
     @Test
-    fun checkInclusionAlt2() {
+    fun checkInclusionLong2() {
         assertTrue(
             solution2.checkInclusion(
                 s1 = "trinitrophenylmethylnitramine",
@@ -85,9 +88,52 @@ class PermutationInAStringTest {
     }
 
     @Test
-    fun checkInclusionLarge2() {
+    fun checkInclusionVeryLong2() {
         assertFalse(
             solution2.checkInclusion(
+                s1 = "zftgfmnoxmfkwufwadd",
+                s2 = longS2
+            )
+        )
+    }
+
+
+    @Test
+    fun checkInclusion3() {
+        assertFalse(solution3.checkInclusion(s1 = "a", s2 = "b"))
+        assertTrue(solution3.checkInclusion(s1 = "ab", s2 = "eidbaooo"))
+        assertFalse(solution3.checkInclusion(s1 = "ab", s2 = "eidboaoo"))
+        assertTrue(solution3.checkInclusion(s1 = "ab", s2 = "zbba"))
+        assertFalse(solution3.checkInclusion(s1 = "ab", s2 = "zbboa"))
+        assertTrue(solution3.checkInclusion(s1 = "abc", s2 = "zabcb"))
+        assertFalse(solution3.checkInclusion(s1 = "abc", s2 = "zabbc"))
+        assertFalse(solution3.checkInclusion(s1 = "xy", s2 = "axbcdefy"))
+        assertTrue(solution3.checkInclusion(s1 = "xy", s2 = "axbcdefyx"))
+        assertFalse(solution3.checkInclusion(s1 = "xxxyyy", s2 = "axxyxybc"))
+        assertTrue(solution3.checkInclusion(s1 = "xxxyyy", s2 = "axxyxyybc"))
+        assertTrue(solution3.checkInclusion(s1 = "adc", s2 = "dcda"))
+    }
+
+    @Test
+    fun checkInclusionLong3() {
+        assertTrue(
+            solution3.checkInclusion(
+                s1 = "trinitrophenylmethylnitramine",
+                s2 = "trinitrophenylmethylnitramine"
+            )
+        )
+        assertTrue(
+            solution3.checkInclusion(
+                s1 = "trinitrophenylmethylnitramine",
+                s2 = "dinitrophenylhydrazinetrinitrophenylmethylnitramine"
+            )
+        )
+    }
+
+    @Test
+    fun checkInclusionVeryLong3() {
+        assertFalse(
+            solution3.checkInclusion(
                 s1 = "zftgfmnoxmfkwufwadd",
                 s2 = longS2
             )
