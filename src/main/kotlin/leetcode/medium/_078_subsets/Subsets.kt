28ee4e218a91277c0c9e3df2c2f109dff1dee058
Navid Rojiani <nrojiani@gmail.com>
2018-09-23 17:37:25 -0400

    Subsets I - More Solutions

--- src/main/kotlin/leetcode/medium/_078_subsets/Subsets.kt
+++ src/main/kotlin/leetcode/medium/_078_subsets/Subsets.kt
@@ -62,7 +62,10 @@ class SolutionFour {
      * Space: O(n)
      */
     fun subsets(nums: IntArray): List<List<Int>> {
-        return powerSet(nums.toSet()).map { it.toList() }.toList()
+        return powerSet(nums.toSet())
+            .asSequence()
+            .map { it.toList() }
+            .toList()
     }
 
     private fun <T> powerSet(set: Set<T>): Set<Set<T>> = when {
@@ -74,7 +77,36 @@ class SolutionFour {
     }
 }
 
+
 class SolutionFive {
+    /**
+     * Backtracking (1 of 2)
+     *
+     * Time: O(2^n)
+     * Space: O(n)
+     */
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val powerSet: MutableList<List<Int>> = arrayListOf()
+        generateSubsets(nums.toList(), arrayListOf(), powerSet)
+        return powerSet
+    }
+
+    private fun generateSubsets(
+        nums: List<Int>,
+        subset: List<Int> = arrayListOf(),
+        powerSet: MutableList<List<Int>>
+    ) {
+        when {
+            nums.isEmpty() -> powerSet += subset
+            else -> {
+                generateSubsets(nums.drop(1), (subset + nums.first()), powerSet)    // include
+                generateSubsets(nums.drop(1), subset, powerSet)                     // don't include
+            }
+        }
+    }
+}
+
+class SolutionSix {
     /**
      * Backtracking
      *
@@ -83,13 +115,43 @@ class SolutionFive {
      */
     fun subsets(nums: IntArray): List<List<Int>> {
         val powerSet = mutableListOf<List<Int>>()
-        backtrack(nums, 0, mutableListOf(), powerSet)
+        backtrack(nums.toList(), 0, mutableListOf(), powerSet)
         return powerSet
     }
 
     private fun backtrack(
-        nums: IntArray,
+        nums: List<Int>,
         startIdx: Int,
+        sublist: MutableList<Int>,
+        powerSet: MutableList<List<Int>>,
+        depth: Int = 0
+    ) {
+        // We want to add the elements in sublist to the powerSet, but we don't want
+        // to add sublist itself into the powerSet since it is mutable
+        powerSet.add(sublist.toList())
+
+        for (i in (startIdx..nums.lastIndex)) {
+            backtrack(nums, i + 1, (sublist + nums[i]).toMutableList(), powerSet, depth + 1)
+        }
+    }
+}
+
+class SolutionSeven {
+    /**
+     * Backtracking - same as [SolutionSix], but without `startIdx` param
+     * (uses more space, but more readable)
+     *
+     * Time: O(2^n)
+     * Space: O(n)
+     */
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val powerSet = mutableListOf<List<Int>>()
+        backtrack(nums.toList(), mutableListOf(), powerSet)
+        return powerSet
+    }
+
+    private fun backtrack(
+        nums: List<Int>,
         sublist: MutableList<Int>,
         powerSet: MutableList<List<Int>>
     ) {
@@ -97,10 +159,48 @@ class SolutionFive {
         // to add sublist itself into the powerSet since it is mutable
         powerSet.add(sublist.toList())
 
-        for (i in (startIdx..nums.lastIndex)) {
-            sublist.add(nums[i])
-            backtrack(nums, i + 1, sublist, powerSet)
-            sublist.removeAt(sublist.lastIndex)
+        nums.forEachIndexed { i, num ->
+            // nums.drop(i + 1): nums[i:]
+            backtrack(
+                nums = nums.drop(i + 1),
+                sublist = (sublist + num).toMutableList(),  // add current num to sublist, which will be added to solution
+                powerSet = powerSet
+            )
         }
+    }
+}
+
+class SolutionEight {
+    /**
+     * Backtracking - same as prev., but without `startIdx` param
+     * (uses more space, but more readable)
+     *
+     * Time: O(2^n)
+     * Space: O(n)
+     */
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val powerSet = mutableListOf<List<Int>>()
+        dfs(nums, 0, mutableListOf(), powerSet)
+        return powerSet
+    }
+
+    private fun dfs(
+        nums: IntArray,
+        index: Int,
+        sublist: MutableList<Int>,
+        powerSet: MutableList<List<Int>>
+    ) {
+        if (index > nums.lastIndex) {
+            powerSet += sublist.toList()
+            return
+        }
+
+        /* Don't include the number */
+        dfs(nums, index + 1, sublist, powerSet)
+
+        /* Include the number */
+        sublist.add(nums[index])
+        dfs(nums, index + 1, sublist, powerSet)
+        sublist.removeAt(sublist.lastIndex) // undo
     }
 }
--- src/test/kotlin/leetcode/medium/_078_subsets/SubsetsTest.kt
+++ src/test/kotlin/leetcode/medium/_078_subsets/SubsetsTest.kt
@@ -10,98 +10,90 @@ class SubsetsTest {
     private val solution3 = SolutionThree()
     private val solution4 = SolutionFour()
     private val solution5 = SolutionFive()
+    private val solution6 = SolutionSix()
+    private val solution7 = SolutionSeven()
+    private val solution8 = SolutionEight()
+
+    private val expected123 = listOf(
+        listOf(3),
+        listOf(1),
+        listOf(2),
+        listOf(1, 2, 3),
+        listOf(1, 3),
+        listOf(2, 3),
+        listOf(1, 2),
+        emptyList()
+    )
 
     @Test
     fun subsets() {
         assertEquals(listOf(emptyList<Int>()), solution.subsets(intArrayOf()))
-        assertEquals(
-            listOf(
-                listOf(3),
-                listOf(1),
-                listOf(2),
-                listOf(1, 2, 3),
-                listOf(1, 3),
-                listOf(2, 3),
-                listOf(1, 2),
-                emptyList()
-            ).toSet(),
-            solution.subsets(intArrayOf(1, 2, 3)).toSet()
-        )
-    }
 
+        val actual123 = solution.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+    }
 
     @Test
     fun subsets2() {
         assertEquals(listOf(emptyList<Int>()), solution2.subsets(intArrayOf()))
-        assertEquals(
-            listOf(
-                listOf(3),
-                listOf(1),
-                listOf(2),
-                listOf(1, 2, 3),
-                listOf(1, 3),
-                listOf(2, 3),
-                listOf(1, 2),
-                emptyList()
-            ).toSet(),
-            solution2.subsets(intArrayOf(1, 2, 3)).toSet()
-        )
-    }
 
+        val actual123 = solution2.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+    }
 
     @Test
     fun subsets3() {
         assertEquals(listOf(emptyList<Int>()), solution3.subsets(intArrayOf()))
-        assertEquals(
-            listOf(
-                listOf(3),
-                listOf(1),
-                listOf(2),
-                listOf(1, 2, 3),
-                listOf(1, 3),
-                listOf(2, 3),
-                listOf(1, 2),
-                emptyList()
-            ).toSet(),
-            solution3.subsets(intArrayOf(1, 2, 3)).toSet()
-        )
+
+        val actual123 = solution3.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
     }
 
     @Test
     fun subsets4() {
         assertEquals(listOf(emptyList<Int>()), solution4.subsets(intArrayOf()))
-        assertEquals(
-            listOf(
-                listOf(3),
-                listOf(1),
-                listOf(2),
-                listOf(1, 2, 3),
-                listOf(1, 3),
-                listOf(2, 3),
-                listOf(1, 2),
-                emptyList()
-            ).toSet(),
-            solution4.subsets(intArrayOf(1, 2, 3)).toSet()
-        )
-    }
 
+        val actual123 = solution4.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+    }
 
     @Test
     fun subsets5() {
         assertEquals(listOf(emptyList<Int>()), solution5.subsets(intArrayOf()))
-        assertEquals(
-            listOf(
-                listOf(3),
-                listOf(1),
-                listOf(2),
-                listOf(1, 2, 3),
-                listOf(1, 3),
-                listOf(2, 3),
-                listOf(1, 2),
-                emptyList()
-            ).toSet(),
-            solution5.subsets(intArrayOf(1, 2, 3)).toSet()
-        )
+
+        val actual123 = solution5.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
     }
 
+    @Test
+    fun subsets6() {
+        assertEquals(listOf(emptyList<Int>()), solution6.subsets(intArrayOf()))
+
+        val actual123 = solution6.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+    }
+
+    @Test
+    fun subsets7() {
+        assertEquals(listOf(emptyList<Int>()), solution7.subsets(intArrayOf()))
+
+        val actual123 = solution7.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+    }
+
+    @Test
+    fun subsets8() {
+        assertEquals(listOf(emptyList<Int>()), solution8.subsets(intArrayOf()))
+
+        val actual123 = solution8.subsets(intArrayOf(1, 2, 3))
+        assertEquals(8, actual123.size)
+        assertEquals(expected123.toSet(), actual123.toSet())
+    }
 }
