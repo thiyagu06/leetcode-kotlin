fd6ae97abb9f4d74685c2d73c805eaefbf1f8296
Navid Rojiani <nrojiani@gmail.com>
2018-09-27 15:26:37 -0400

    Group Anagrams

--- src/main/kotlin/leetcode/medium/_049_group_anagrams/GroupAnagrams.kt
+++ src/main/kotlin/leetcode/medium/_049_group_anagrams/GroupAnagrams.kt
@@ -1,9 +1,22 @@
 package leetcode.medium._049_group_anagrams
 
+import extensions.strings.toSortedString
+
 /**
  * 49 - https://leetcode.com/problems/group-anagrams/
  */
 class Solution {
+    /**
+     * Time: O(N * K * log (K)), where N = strs.size, K = max. length of a string in strs.
+     * Space: O(N * K) -  the total information content stored in the map
+     */
+    fun groupAnagrams(strs: Array<String>): List<List<String>> =
+        strs.groupBy { it.toSortedString() }
+            .values
+            .toList()
+}
+
+class SolutionTwo {
     /**
      * Time: O(N * K * log (K)), where N = strs.size, K = max. length of a string in strs.
      *       The outer loop has complexity O(N) as we iterate through each string. Then, we sort each string in
@@ -14,9 +27,9 @@ class Solution {
         val wordAndLetters = strs.map { (it to it.toCharArray().sorted()) }     // [(eat, [a, e, t]), ... ]
 
         return wordAndLetters.groupingBy { (word, letters) -> letters }
-                .fold(emptyList<String>()) { acc, (word, letters) ->
-                    acc + word
-                }.values
-                .map { anagramList -> anagramList.sorted() }
+            .fold(emptyList<String>()) { acc, (word, letters) ->
+                acc + word
+            }.values
+            .toList()
     }
 }
--- src/test/kotlin/leetcode/medium/_049_group_anagrams/GroupAnagramsTest.kt
+++ src/test/kotlin/leetcode/medium/_049_group_anagrams/GroupAnagramsTest.kt
@@ -1,21 +1,148 @@
 package leetcode.medium._049_group_anagrams
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
-import kotlin.system.measureTimeMillis
 
 class GroupAnagramsTest {
 
     private val solution = Solution()
+    private val solution2 = SolutionTwo()
+    private val largeInput = arrayOf(
+        "hos", "boo", "nay", "deb", "wow", "bop", "bob", "brr", "hey", "rye", "eve", "elf", "pup", "bum",
+        "iva", "lyx", "yap", "ugh", "hem", "rod", "aha", "nam", "gap", "yea", "doc", "pen", "job", "dis",
+        "max", "oho", "jed", "lye", "ram", "pup", "qua", "ugh", "mir", "nap", "deb", "hog", "let", "gym",
+        "bye", "lon", "aft", "eel", "sol", "jab"
+    )
 
     @Test
     fun groupAnagrams() {
-        val ms = measureTimeMillis {
-            assertEquals(
-                    listOf(listOf("ate", "eat","tea"), listOf("nat","tan"), listOf("bat")),
-                    solution.groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat")))
+        val expected = listOf(listOf("ate", "eat", "tea"), listOf("nat", "tan"), listOf("bat"))
+        val actual = solution.groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
+            .map { it.sorted() }
+
+        assertEquals(expected.size, actual.size)
+        expected.forEach { sublist ->
+            assertTrue(sublist in actual)
         }
-        println("$ms ms")
+
+        val expectedLargeInputResult = listOf(
+            listOf("sol"),
+            listOf("wow"),
+            listOf("gap"),
+            listOf("hem"),
+            listOf("yap"),
+            listOf("bum"),
+            listOf("ugh", "ugh"),
+            listOf("aha"),
+            listOf("jab"),
+            listOf("eve"),
+            listOf("bop"),
+            listOf("lyx"),
+            listOf("jed"),
+            listOf("iva"),
+            listOf("rod"),
+            listOf("boo"),
+            listOf("brr"),
+            listOf("hog"),
+            listOf("nay"),
+            listOf("mir"),
+            listOf("deb", "deb"),
+            listOf("aft"),
+            listOf("dis"),
+            listOf("yea"),
+            listOf("hos"),
+            listOf("rye"),
+            listOf("hey"),
+            listOf("doc"),
+            listOf("bob"),
+            listOf("eel"),
+            listOf("pen"),
+            listOf("job"),
+            listOf("max"),
+            listOf("oho"),
+            listOf("lye"),
+            listOf("ram"),
+            listOf("nap"),
+            listOf("elf"),
+            listOf("qua"),
+            listOf("pup", "pup"),
+            listOf("let"),
+            listOf("gym"),
+            listOf("nam"),
+            listOf("bye"),
+            listOf("lon")
+        )
+        val actualLargeResult = solution.groupAnagrams(largeInput).map { it.sorted() }
+        assertEquals(expectedLargeInputResult.size, actualLargeResult.size)
+        expectedLargeInputResult.forEach { sublist ->
+            assertTrue(sublist in actualLargeResult)
+        }
     }
+
+    @Test
+    fun groupAnagrams2() {
+        val expected = listOf(listOf("ate", "eat", "tea"), listOf("nat", "tan"), listOf("bat"))
+        val actual = solution2.groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
+            .map { it.sorted() }
+        assertEquals(expected.size, actual.size)
+        expected.forEach { sublist ->
+            assertTrue(sublist in actual)
+        }
+
+        val expectedLargeInputResult = listOf(
+            listOf("sol"),
+            listOf("wow"),
+            listOf("gap"),
+            listOf("hem"),
+            listOf("yap"),
+            listOf("bum"),
+            listOf("ugh", "ugh"),
+            listOf("aha"),
+            listOf("jab"),
+            listOf("eve"),
+            listOf("bop"),
+            listOf("lyx"),
+            listOf("jed"),
+            listOf("iva"),
+            listOf("rod"),
+            listOf("boo"),
+            listOf("brr"),
+            listOf("hog"),
+            listOf("nay"),
+            listOf("mir"),
+            listOf("deb", "deb"),
+            listOf("aft"),
+            listOf("dis"),
+            listOf("yea"),
+            listOf("hos"),
+            listOf("rye"),
+            listOf("hey"),
+            listOf("doc"),
+            listOf("bob"),
+            listOf("eel"),
+            listOf("pen"),
+            listOf("job"),
+            listOf("max"),
+            listOf("oho"),
+            listOf("lye"),
+            listOf("ram"),
+            listOf("nap"),
+            listOf("elf"),
+            listOf("qua"),
+            listOf("pup", "pup"),
+            listOf("let"),
+            listOf("gym"),
+            listOf("nam"),
+            listOf("bye"),
+            listOf("lon")
+        )
+        val actualLargeResult = solution2.groupAnagrams(largeInput).map { it.sorted() }
+        assertEquals(expectedLargeInputResult.size, actualLargeResult.size)
+        expectedLargeInputResult.forEach { sublist ->
+            assertTrue(sublist in actualLargeResult)
+        }
+    }
+
+
 }
