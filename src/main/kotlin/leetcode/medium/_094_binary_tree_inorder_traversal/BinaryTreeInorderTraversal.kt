98fe9e85a46feb637dacddbdac4e223831932e6b
Navid Rojiani <nrojiani@gmail.com>
2018-09-27 15:27:00 -0400

    Binary Tree Iterative Inorder Traversal

--- src/main/kotlin/leetcode/medium/_094_binary_tree_inorder_traversal/BinaryTreeInorderTraversal.kt
+++ src/main/kotlin/leetcode/medium/_094_binary_tree_inorder_traversal/BinaryTreeInorderTraversal.kt
@@ -1,16 +1,97 @@
 package leetcode.medium._094_binary_tree_inorder_traversal
 
 import datastructures.tree.TreeNode
+import datastructures.tree.hasLeft
+import java.util.*
 
 /**
  * 94 - https://leetcode.com/problems/binary-tree-inorder-traversal/
  */
 class Solution {
+    /**
+     * Recursive
+     * Time: O(n)
+     * Space: O(n)
+     */
     fun inorderTraversal(root: TreeNode?, values: MutableList<Int> = arrayListOf()): List<Int> =
             root?.let {
                 root.left?.let { left -> inorderTraversal(left, values) }
                 values += root.`val`
                 root.right?.let { right -> inorderTraversal(right, values) }
             } ?: values
+}
 
+class SolutionTwo {
+    /**
+     * Iterative
+     * http://tinyurl.com/y7s227k9
+     *
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val visited: MutableList<Int> = arrayListOf()
+        val stack = Stack<TreeNode?>()
+        var node: TreeNode? = root
+
+        while (node != null || stack.isNotEmpty()) {
+            /* Push left children */
+            while (node != null) {
+                stack.push(node)
+                node = node.left
+            }
+
+            node = stack.pop()
+            node?.let {
+                visited += it.`val`
+            }
+            node = node?.right
+        }
+
+        return visited
+    }
+}
+
+class DebugSolutionTwo {
+    /**
+     * Iterative
+     * http://tinyurl.com/y7s227k9
+     *
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val visited: MutableList<Int> = arrayListOf()
+        val stack = Stack<TreeNode?>()
+        var node: TreeNode? = root
+
+        while (node != null || stack.isNotEmpty()) {
+            println("\nouter loop: node: $node, stack: $stack, visited: $visited")
+            /* Push left children */
+            val debugStackSizeBefore = stack.size
+            if (node != null) println("1. inner loop (push all left children onto stack)")
+            else println("1. node is null -> skip inner loop")
+            while (node != null) {
+                stack.push(node)
+                node = node.left
+            }
+            if (stack.size > debugStackSizeBefore) {
+                println("Stack after pushing all L children: $stack")
+            }
+
+            // Node will be null. Top of stack has lowest L child.
+            println("2. Pop stack: node = ${stack.peek()}")
+            node = stack.pop()
+            node?.let {
+                println("visited: $visited -> ${visited + it.`val`}")
+                visited += it.`val`
+            }
+            println("3. Go right: node = node?.right: $node -> ${node?.right}")
+            node = node?.right
+            println("end outer loop")
+        }
+
+        println("END\n==========================\n")
+        return visited
+    }
 }
--- src/test/kotlin/leetcode/medium/_094_binary_tree_inorder_traversal/BinaryTreeInorderTraversalTest.kt
+++ src/test/kotlin/leetcode/medium/_094_binary_tree_inorder_traversal/BinaryTreeInorderTraversalTest.kt
@@ -8,10 +8,24 @@ import org.junit.Test
 class BinaryTreeInorderTraversalTest {
 
     private val solution = Solution()
+    private val solution2 = SolutionTwo()
+    private val debugSolution2 = DebugSolutionTwo()
 
     @Test
     fun inorderTraversal() {
         assertEquals(listOf(1, 3, 4, 5, 6, 8, 9), solution.inorderTraversal(bst3LevelsFull))
         assertEquals(listOf(1, 3, 4, 7, 8, 9), solution.inorderTraversal(bstWithNulls))
+    }
+
+    @Test
+    fun inorderTraversal2() {
+        assertEquals(listOf(1, 3, 4, 5, 6, 8, 9), solution2.inorderTraversal(bst3LevelsFull))
+        assertEquals(listOf(1, 3, 4, 7, 8, 9), solution2.inorderTraversal(bstWithNulls))
+    }
+
+    @Test
+    fun inorderTraversalDebug2() {
+        assertEquals(listOf(1, 3, 4, 5, 6, 8, 9), debugSolution2.inorderTraversal(bst3LevelsFull))
+        assertEquals(listOf(1, 3, 4, 7, 8, 9), debugSolution2.inorderTraversal(bstWithNulls))
     }
 }
