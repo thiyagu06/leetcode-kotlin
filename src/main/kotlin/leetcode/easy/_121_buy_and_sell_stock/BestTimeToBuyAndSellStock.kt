e412f2c8a4e37c96ee8e60cebca54b5db53f9e2f
Navid Rojiani <nrojiani@gmail.com>
2018-10-08 15:48:17 -0400

    DP: Best Time to Buy & Sell Stock

--- src/main/kotlin/leetcode/easy/_121_buy_and_sell_stock/BestTimeToBuyAndSellStock.kt
+++ src/main/kotlin/leetcode/easy/_121_buy_and_sell_stock/BestTimeToBuyAndSellStock.kt
@@ -1,2 +1,68 @@
 package leetcode.easy._121_buy_and_sell_stock
 
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * 121 - https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+ */
+class Solution {
+    /**
+     * Brute-force solution (Accepted)
+     * Time: O(n^2)
+     * Space: O(n)
+     */
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        (0 until prices.lastIndex).forEach { i ->
+            ((i + 1)..prices.lastIndex).forEach { j ->
+                val profit = prices[j] - prices[i]
+                maxProfit = max(maxProfit, profit)
+            }
+        }
+        return maxProfit
+    }
+}
+
+class SolutionTwo {
+    /**
+     * Dynamic Programming solution
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun maxProfit(prices: IntArray): Int {
+        if (prices.size < 2) return 0
+
+        /* maxProfits[i] = the max profit for prices[0..i] */
+        var maxProfits = IntArray(prices.size)
+        maxProfits[0] = 0
+        maxProfits[1] = max(prices[1] - prices[0], maxProfits[0])
+        var minBuyPrice = prices[0]
+
+        for (i in 1..prices.lastIndex) {
+            minBuyPrice = min(minBuyPrice, prices[i])
+            maxProfits[i] = max(maxProfits[i - 1], prices[i] - minBuyPrice)
+        }
+        return maxProfits.last()
+    }
+}
+
+class SolutionThree {
+    /**
+     * Dynamic Programming solution, optimize space
+     * Time: O(n)
+     * Space: O(1)
+     */
+    fun maxProfit(prices: IntArray): Int {
+        if (prices.size < 2) return 0
+
+        var maxProfit = 0
+        var minBuyPrice = prices[0]
+
+        (1..prices.lastIndex).forEach { i ->
+            minBuyPrice = min(minBuyPrice, prices[i])
+            maxProfit = max(maxProfit, prices[i] - minBuyPrice)
+        }
+        return maxProfit
+    }
+}
--- src/test/kotlin/leetcode/easy/_121_buy_and_sell_stock/BestTimeToBuyAndSellStockTest.kt
+++ src/test/kotlin/leetcode/easy/_121_buy_and_sell_stock/BestTimeToBuyAndSellStockTest.kt
@@ -1,10 +1,38 @@
 package leetcode.easy._121_buy_and_sell_stock
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class BestTimeToBuyAndSellStockTest {
 
-    @org.junit.Test
+    private val solution = Solution()
+    private val solution2 = SolutionTwo()
+    private val solution3 = SolutionThree()
+
+    @Test
     fun maxProfit() {
+        assertEquals(5, solution.maxProfit(intArrayOf(7, 1, 5, 3, 6, 4)))
+        assertEquals(0, solution.maxProfit(intArrayOf(7, 6, 4, 3, 1)))
+        assertEquals(0, solution.maxProfit(intArrayOf(3, 3, 3, 3)))
+        assertEquals(0, solution.maxProfit(intArrayOf(3)))
+        assertEquals(0, solution.maxProfit(intArrayOf()))
+    }
+
+    @Test
+    fun maxProfit2() {
+        assertEquals(5, solution2.maxProfit(intArrayOf(7, 1, 5, 3, 6, 4)))
+        assertEquals(0, solution2.maxProfit(intArrayOf(7, 6, 4, 3, 1)))
+        assertEquals(0, solution2.maxProfit(intArrayOf(3, 3, 3, 3)))
+        assertEquals(0, solution2.maxProfit(intArrayOf(3)))
+        assertEquals(0, solution2.maxProfit(intArrayOf()))
+    }
+
+    @Test
+    fun maxProfit3() {
+        assertEquals(5, solution3.maxProfit(intArrayOf(7, 1, 5, 3, 6, 4)))
+        assertEquals(0, solution3.maxProfit(intArrayOf(7, 6, 4, 3, 1)))
+        assertEquals(0, solution3.maxProfit(intArrayOf(3, 3, 3, 3)))
+        assertEquals(0, solution3.maxProfit(intArrayOf(3)))
+        assertEquals(0, solution3.maxProfit(intArrayOf()))
     }
 }
