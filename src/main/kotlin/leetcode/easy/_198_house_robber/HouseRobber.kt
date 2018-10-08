5b63db688e938636354e95c5afb9ae0ed464d1db
Navid Rojiani <nrojiani@gmail.com>
2018-10-08 15:02:19 -0400

    DP: House Robber

--- src/main/kotlin/leetcode/easy/_198_house_robber/HouseRobber.kt
+++ src/main/kotlin/leetcode/easy/_198_house_robber/HouseRobber.kt
@@ -1,11 +1,15 @@
 package leetcode.easy._198_house_robber
 
+import kotlin.math.max
+
 /**
  * 198 - https://leetcode.com/problems/house-robber/
  */
 class Solution {
 
     /**
+     * Adjusting indices
+     *
      * Time: O(n)
      * Space: O(n)
      */
@@ -16,11 +20,63 @@ class Solution {
         val houses = intArrayOf(0) + nums
 
         maxValues[0] = 0
-        maxValues[1] = houses[1]
+        maxValues[1] = houses[1]    // nums[0]
         for (i in 2..houses.lastIndex) {
-            maxValues[i] = maxOf( maxValues[i - 1], houses[i] + maxValues[i - 2] )
+            maxValues[i] = max(maxValues[i - 1], houses[i] + maxValues[i - 2])
         }
 
         return maxValues.last()
+    }
+}
+
+class SolutionTwo {
+
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
+    fun rob(nums: IntArray): Int = when (nums.size) {
+        0 -> 0
+        1 -> nums[0]
+        2 -> max(nums[0], nums[1])
+        else -> {
+            val maxes = IntArray(nums.size)     // maxes[i]: optimum for nums[0..i]
+            maxes[0] = nums[0]
+            maxes[1] = max(nums[0], nums[1])
+
+            for (i in 2..nums.lastIndex) {
+                maxes[i] = max(maxes[i - 2] + nums[i], maxes[i - 1])
+            }
+
+            maxes.last()
+        }
+    }
+}
+
+class SolutionThree {
+
+    /**
+     * Constant space
+     *
+     * Time: O(n)
+     * Space: O(1)
+     */
+    fun rob(nums: IntArray): Int = when (nums.size) {
+        0 -> 0
+        1 -> nums[0]
+        2 -> max(nums[0], nums[1])
+        else -> {
+            var maxTwoHousesBack = nums[0]
+            var maxPrevHouse = max(nums[0], nums[1])
+            var maxCurrHouse = maxPrevHouse
+
+            for (i in 2..nums.lastIndex) {
+                maxCurrHouse = max(maxTwoHousesBack + nums[i], maxPrevHouse)
+                maxTwoHousesBack = maxPrevHouse
+                maxPrevHouse = maxCurrHouse
+            }
+
+            maxCurrHouse
+        }
     }
 }
--- src/test/kotlin/leetcode/easy/_198_house_robber/HouseRobberTest.kt
+++ src/test/kotlin/leetcode/easy/_198_house_robber/HouseRobberTest.kt
@@ -6,11 +6,35 @@ import org.junit.Assert.*
 
 class HouseRobberTest {
     private val solution = Solution()
+    private val solution2 = SolutionTwo()
+    private val solution3 = SolutionThree()
+
+    private val expected = mapOf(
+        intArrayOf(2, 4, 2, 4) to 8,
+        intArrayOf(2, 1, 1, 2) to 4,
+        intArrayOf(2, 7, 9, 3, 1) to 12,
+        intArrayOf(2, 7, 9, 99, 1) to 106
+    )
 
     @Test
     fun rob() {
-        assertEquals(8, solution.rob(intArrayOf(2, 4, 2, 4)))
-        assertEquals(4, solution.rob(intArrayOf(2, 1, 1, 2)))
+        expected.forEach { (input, output) ->
+            assertEquals(output, solution.rob(input))
+        }
+    }
+
+    @Test
+    fun rob2() {
+        expected.forEach { (input, output) ->
+            assertEquals(output, solution2.rob(input))
+        }
+    }
+
+    @Test
+    fun rob3() {
+        expected.forEach { (input, output) ->
+            assertEquals(output, solution3.rob(input))
+        }
     }
 
 }
