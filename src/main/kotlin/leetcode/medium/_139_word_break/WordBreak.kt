300048eed650b143922f4006063b503045befe48
Navid Rojiani <nrojiani@gmail.com>
2018-10-15 21:02:38 -0400

    DP: Word Break I

--- src/main/kotlin/leetcode/medium/_139_word_break/WordBreak.kt
+++ src/main/kotlin/leetcode/medium/_139_word_break/WordBreak.kt
@@ -4,36 +4,42 @@ package leetcode.medium._139_word_break
  * 139 - https://leetcode.com/problems/word-break/
  */
 class Solution {
+    /**
+     * Brute-force, without memoization.
+     * Time: O(2^n)
+     * Space: O(n)
+     */
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
-        val cache = mutableMapOf<String, Boolean>()
-        wordDict.forEach { word ->
-            cache[word] = true
+        if (s.isEmpty()) return true
+
+        val matchingPrefixes = wordDict.filter { s.startsWith(it) }
+
+        return matchingPrefixes.any { prefix ->
+            val afterPrefix = s.drop(prefix.length)
+            wordBreak(afterPrefix, wordDict)
         }
-        return wordBreak(unmatched = s, wordDict = wordDict.toSet(), cache = cache)
     }
+}
 
-    private fun wordBreak(unmatched: String, wordDict: Set<String>, cache: MutableMap<String, Boolean> = mutableMapOf()): Boolean {
-        if (unmatched.isEmpty()) {
-            cache[unmatched] = true
-            return true
-        }
+class SolutionTwo {
+    /**
+     * With memoization.
+     * Time: O(n^3)
+     * Space: O(n^2)
+     */
+    fun wordBreak(s: String, wordDict: List<String>): Boolean = wordBreak(s, wordDict, hashMapOf())
 
-        val prefixMatches = wordDict.filter { unmatched.startsWith(it) }
-        prefixMatches.forEach { prefix ->
-            val afterPrefix = unmatched.drop(prefix.length)
+    private fun wordBreak(s: String, wordDict: List<String>, cache: MutableMap<String, Boolean>): Boolean {
+        if (s.isEmpty()) return true
 
-            when (cache[afterPrefix]) {
-                true -> return true
-                false -> return@forEach
-                else -> {
-                    val isSolution = wordBreak(unmatched = afterPrefix, wordDict = wordDict, cache = cache)
-                    cache[afterPrefix] = isSolution
+        val matchingPrefixes = wordDict.filter { s.startsWith(it) }
 
-                    if (isSolution) return true
-                }
+        return matchingPrefixes.any { prefix ->
+            val afterPrefix = s.drop(prefix.length)
+            if (!cache.contains(afterPrefix)) {
+                cache[afterPrefix] = wordBreak(afterPrefix, wordDict, cache)
             }
+            cache[afterPrefix]!!
         }
-
-        return false
     }
 }
--- src/test/kotlin/leetcode/medium/_139_word_break/WordBreakTest.kt
+++ src/test/kotlin/leetcode/medium/_139_word_break/WordBreakTest.kt
@@ -2,11 +2,16 @@ package leetcode.medium._139_word_break
 
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Test
 
 class WordBreakTest {
 
     private val solution = Solution()
+    private val solution2 = SolutionTwo()
+    private val bigInputString = "a".repeat(150) + 'b'
+    private val bigInputWordDict =
+        listOf("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")
 
     @Test
     fun wordBreak() {
@@ -34,10 +39,58 @@ class WordBreakTest {
 
         /* Multiple valid solutions */
         assertTrue(solution.wordBreak(s = "leetcode", wordDict = listOf("leet", "code", "leetc", "ode")))
+        assertTrue(solution.wordBreak(s = "ilikesamsung", wordDict = listOf("i", "like", "sam", "sung", "samsung", "mobile", "ice",
+            "cream", "icecream", "man", "go", "mango")))
 
         /* More than 1 first selection, but only 1 yields an answer */
         assertTrue(solution.wordBreak(s = "leetcode", wordDict = listOf("leetc", "od", "leet", "code")))
         assertTrue(solution.wordBreak(s = "leetcode", wordDict = listOf("leet", "code", "leetc", "od")))
         assertTrue(solution.wordBreak(s = "leetcoder", wordDict = listOf("leet", "coder", "leetc", "ode")))
+    }
+
+    @Ignore // Brute-force, un-memoized
+    @Test
+    fun wordBreakBigInput() {
+        assertFalse(solution.wordBreak(bigInputString, bigInputWordDict))
+    }
+
+    @Test
+    fun wordBreak2() {
+        assertTrue(solution2.wordBreak(s = "a", wordDict = listOf("a")))
+        assertFalse(solution2.wordBreak(s = "leetcode", wordDict = listOf("leet")))
+        assertTrue(solution2.wordBreak(s = "leetcode", wordDict = listOf("leet", "code")))
+        assertTrue(solution2.wordBreak(s = "leetcode", wordDict = listOf("leet", "code", "ninja")))
+        assertTrue(solution2.wordBreak(s = "leetcodeninja", wordDict = listOf("leet", "code", "ninja")))
+        assertTrue(solution2.wordBreak(s = "leetcode", wordDict = listOf("code", "leet")))
+
+        assertTrue(solution2.wordBreak(s = "applepenapple", wordDict = listOf("apple", "pen")))
+        assertFalse(solution2.wordBreak(s = "applepenfoob", wordDict = listOf("apple", "pen")))
+        assertFalse(solution2.wordBreak(s = "catsandog", wordDict = listOf("cats", "dog", "sand", "and", "cat")))
+        assertTrue(solution2.wordBreak(s = "catsandog", wordDict = listOf("cats", "dog", "sand", "an", "cat")))
+
+        /* Can a word be used more than once? => Yes */
+        assertTrue(solution2.wordBreak(s = "aa", wordDict = listOf("a")))
+        assertTrue(solution2.wordBreak(s = "leetleetcode", wordDict = listOf("leet", "code")))
+
+        /* Are digits, punctuation, whitespace, etc. allowed? => Yes */
+        assertTrue(solution2.wordBreak(s = "$1.99", wordDict = listOf("$1", ".99")))
+        assertTrue(solution2.wordBreak(s = "coca-cola$1.99", wordDict = listOf("coca", "cola", "-", "$1", ".99")))
+        assertTrue(solution2.wordBreak(s = "coca-cola $1.99", wordDict = listOf("coca", "cola", " ", "-", "$1", ".99")))
+        assertFalse(solution2.wordBreak(s = "coca-cola $1.99", wordDict = listOf("coca", "cola", "-", "$1", ".99")))
+
+        /* Multiple valid solutions */
+        assertTrue(solution2.wordBreak(s = "leetcode", wordDict = listOf("leet", "code", "leetc", "ode")))
+        assertTrue(solution2.wordBreak(s = "ilikesamsung", wordDict = listOf("i", "like", "sam", "sung", "samsung", "mobile", "ice",
+            "cream", "icecream", "man", "go", "mango")))
+
+        /* More than 1 first selection, but only 1 yields an answer */
+        assertTrue(solution2.wordBreak(s = "leetcode", wordDict = listOf("leetc", "od", "leet", "code")))
+        assertTrue(solution2.wordBreak(s = "leetcode", wordDict = listOf("leet", "code", "leetc", "od")))
+        assertTrue(solution2.wordBreak(s = "leetcoder", wordDict = listOf("leet", "coder", "leetc", "ode")))
+    }
+
+    @Test
+    fun wordBreakBigInput2() {
+        assertFalse(solution2.wordBreak(bigInputString, bigInputWordDict))
     }
 }
