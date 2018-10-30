61513029aca7ec669728704b1aec8cee55b04628
Navid Rojiani <nrojiani@gmail.com>
2018-10-30 17:47:38 -0400

    Combination Sum

--- src/main/kotlin/leetcode/medium/_039_combination_sum/CombinationSum.kt
+++ src/main/kotlin/leetcode/medium/_039_combination_sum/CombinationSum.kt
@@ -4,6 +4,10 @@ package leetcode.medium._039_combination_sum
  * 39 - https://leetcode.com/problems/combination-sum/
  */
 class Solution {
+    /**
+     * Time: O(?)
+     * Space: O(?)
+     */
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
         val solutionSet = arrayListOf<List<Int>>()
         combinationSum(candidates.toList(), target, listOf(), solutionSet)
@@ -18,51 +22,109 @@ class Solution {
     ) {
         when {
             target < 0 || candidates.isEmpty() -> return
-            target == 0 -> {
-                solutionSet += currentSolution
-            }
+            target == 0 -> solutionSet += currentSolution
             else -> {
+                // Don't include
                 combinationSum(
-                    candidates = candidates.drop(1), target = target,
-                    currentSolution = currentSolution, solutionSet = solutionSet
+                    candidates = candidates.drop(1),
+                    target = target,
+                    currentSolution = currentSolution,
+                    solutionSet = solutionSet
                 )
 
+                // Include
                 combinationSum(
-                    candidates = candidates, target = target - candidates.first(),
-                    currentSolution = currentSolution + candidates.first(), solutionSet = solutionSet
+                    candidates = candidates,
+                    target = target - candidates.first(),
+                    currentSolution = currentSolution + candidates.first(),
+                    solutionSet = solutionSet
                 )
             }
         }
     }
 }
 
+class SolutionTwo {
+    /**
+     * Similar to above, but uses less space
+     * Time: O(?)
+     * Space: O(?)
+     */
+    fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+        val solutions: MutableSet<List<Int>> = hashSetOf()
+        combinationSum(
+            candidates = candidates,
+            index = 0,
+            remaining = target,
+            currentSolution = emptyList(),
+            solutions = solutions
+        )
+        return solutions.toList()
+    }
 
-class Solution2 {
+    private fun combinationSum(
+        candidates: IntArray,
+        index: Int,
+        remaining: Int,
+        currentSolution: List<Int>,
+        solutions: MutableSet<List<Int>>
+    ) {
+        when {
+            remaining < 0 || index > candidates.lastIndex -> return
+            remaining == 0 -> solutions += currentSolution
+            remaining > 0 -> {
+                // Include (possibly repeat)
+                if (!solutions.contains(currentSolution + candidates[index])) {
+                    combinationSum(
+                        candidates = candidates,
+                        index = index,
+                        remaining = remaining - candidates[index],
+                        currentSolution = currentSolution + candidates[index],
+                        solutions = solutions
+                    )
+                }
+
+                // Don't include
+                combinationSum(
+                    candidates = candidates,
+                    index = index + 1,
+                    remaining = remaining,
+                    currentSolution = currentSolution,
+                    solutions = solutions
+                )
+            }
+        }
+    }
+}
+
+class SolutionThree {
+    /**
+     * Time: O(?)
+     * Space: O(?)
+     */
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
         val solutionSet = hashSetOf<List<Int>>()
-        backtrack(solutionSet, arrayListOf(), candidates.toList(), target)
+        backtrack(candidates.toList(), target, listOf(), solutionSet)
         return solutionSet.toList()
     }
 
     private fun backtrack(
-        solutionSet: MutableSet<List<Int>>,
-        currentSolution: List<Int>,
         candidates: List<Int>,
-        remaining: Int
+        remaining: Int,
+        currentSolution: List<Int>,
+        solutionSet: MutableSet<List<Int>>
     ) {
         when {
             remaining < 0 -> return
-            remaining == 0 -> {
-                solutionSet += currentSolution
-            }
+            remaining == 0 -> solutionSet += currentSolution
             else ->
                 candidates.forEachIndexed { i, n  ->
                     if (!solutionSet.contains(currentSolution + n)) {
                         backtrack(
-                            solutionSet = solutionSet,
-                            currentSolution = currentSolution + n,
                             candidates = candidates.drop(i),
-                            remaining = remaining - n
+                            remaining = remaining - n,
+                            currentSolution = currentSolution + n,
+                            solutionSet = solutionSet
                         )
                     }
                 }
--- src/test/kotlin/leetcode/medium/_039_combination_sum/CombinationSumTest.kt
+++ src/test/kotlin/leetcode/medium/_039_combination_sum/CombinationSumTest.kt
@@ -8,25 +8,33 @@ import kotlin.system.measureTimeMillis
 class CombinationSumTest {
 
     private val solution = Solution()
-    private val solution2 = Solution2()
+    private val solution2 = SolutionTwo()
+    private val solution3 = SolutionThree()
 
     @Test
     fun combinationSum() {
         val times = arrayListOf<Long>()
         repeat(30) {
             val ms = measureTimeMillis {
-                assertEquals(listOf(listOf(7), listOf(2, 2, 3)).toSet(),
-                    solution.combinationSum(intArrayOf(2, 3, 6, 7), 7).toSet())
-                assertEquals(listOf(listOf(1, 1, 1), listOf(1, 2), listOf(3)).toSet(), solution.combinationSum(intArrayOf(1, 2, 3), 3).toSet())
-                assertEquals(
-                    listOf(listOf(2, 2, 2, 2), listOf(2, 3, 3), listOf(3, 5)).toSet(),
-                    solution.combinationSum(intArrayOf(2, 3, 5), 8).toSet()
-                )
+                val expected1 = listOf(listOf(7), listOf(2, 2, 3))
+                val actual1 = solution.combinationSum(intArrayOf(2, 3, 6, 7), 7)
+                assertEquals(expected1.size, actual1.size)
+                assertEquals(expected1.toSet(), actual1.toSet())
+
+                val expected2 = listOf(listOf(1, 1, 1), listOf(1, 2), listOf(3))
+                val actual2 = solution.combinationSum(intArrayOf(1, 2, 3), 3)
+                assertEquals(expected2.size, actual2.size)
+                assertEquals(expected2.toSet(), actual2.toSet())
+
+                val expected3 = listOf(listOf(2, 2, 2, 2), listOf(2, 3, 3), listOf(3, 5))
+                val actual3 = solution.combinationSum(intArrayOf(2, 3, 5), 8)
+                assertEquals(expected3.size, actual3.size)
+                assertEquals(expected3.toSet(), actual3.toSet())
             }
             times += ms
         }
-        val adjustedTimes = times.drop(5)   // 1st one or two affected by test startup
-        println("adjustedTimes for A: $adjustedTimes")
+        val adjustedTimes = times.drop(5)
+        println("adjustedTimes for Solution 1: $adjustedTimes")
         println("average run: ${adjustedTimes.average().roundedToNDecimalPlaces(2)}")
     }
 
@@ -35,17 +43,52 @@ class CombinationSumTest {
         val times = arrayListOf<Long>()
         repeat(30) {
             val ms = measureTimeMillis {
-                assertEquals(listOf(listOf(7), listOf(2, 2, 3)).toSet(), solution2.combinationSum(intArrayOf(2, 3, 6, 7), 7).toSet())
-                assertEquals(listOf(listOf(1, 1, 1), listOf(1, 2), listOf(3)).toSet(), solution2.combinationSum(intArrayOf(1, 2, 3), 3).toSet())
-                assertEquals(
-                    listOf(listOf(2, 2, 2, 2), listOf(2, 3, 3), listOf(3, 5)).toSet(),
-                    solution2.combinationSum(intArrayOf(2, 3, 5), 8).toSet()
-                )
+                val expected1 = listOf(listOf(7), listOf(2, 2, 3))
+                val actual1 = solution2.combinationSum(intArrayOf(2, 3, 6, 7), 7)
+                assertEquals(expected1.size, actual1.size)
+                assertEquals(expected1.toSet(), actual1.toSet())
+
+                val expected2 = listOf(listOf(1, 1, 1), listOf(1, 2), listOf(3))
+                val actual2 = solution2.combinationSum(intArrayOf(1, 2, 3), 3)
+                assertEquals(expected2.size, actual2.size)
+                assertEquals(expected2.toSet(), actual2.toSet())
+
+                val expected3 = listOf(listOf(2, 2, 2, 2), listOf(2, 3, 3), listOf(3, 5))
+                val actual3 = solution2.combinationSum(intArrayOf(2, 3, 5), 8)
+                assertEquals(expected3.size, actual3.size)
+                assertEquals(expected3.toSet(), actual3.toSet())
             }
             times += ms
         }
-        val adjustedTimes = times.drop(5)   // 1st one or two affected by test startup
-        println("adjustedTimes for B: $adjustedTimes")
+        val adjustedTimes = times.drop(5)
+        println("adjustedTimes for Solution 2: $adjustedTimes")
+        println("average run: ${adjustedTimes.average().roundedToNDecimalPlaces(2)}")
+    }
+
+    @Test
+    fun combinationSum3() {
+        val times = arrayListOf<Long>()
+        repeat(30) {
+            val ms = measureTimeMillis {
+                val expected1 = listOf(listOf(7), listOf(2, 2, 3))
+                val actual1 = solution3.combinationSum(intArrayOf(2, 3, 6, 7), 7)
+                assertEquals(expected1.size, actual1.size)
+                assertEquals(expected1.toSet(), actual1.toSet())
+
+                val expected2 = listOf(listOf(1, 1, 1), listOf(1, 2), listOf(3))
+                val actual2 = solution3.combinationSum(intArrayOf(1, 2, 3), 3)
+                assertEquals(expected2.size, actual2.size)
+                assertEquals(expected2.toSet(), actual2.toSet())
+
+                val expected3 = listOf(listOf(2, 2, 2, 2), listOf(2, 3, 3), listOf(3, 5))
+                val actual3 = solution3.combinationSum(intArrayOf(2, 3, 5), 8)
+                assertEquals(expected3.size, actual3.size)
+                assertEquals(expected3.toSet(), actual3.toSet())
+            }
+            times += ms
+        }
+        val adjustedTimes = times.drop(5)
+        println("adjustedTimes for Solution 3: $adjustedTimes")
         println("average run: ${adjustedTimes.average().roundedToNDecimalPlaces(2)}")
     }
 }
